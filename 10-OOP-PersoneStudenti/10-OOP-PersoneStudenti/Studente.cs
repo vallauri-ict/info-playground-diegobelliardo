ab2c4cee91bbc4d9beb7a88c2d0fd8a2922c6eb9
diegobelliardo <57402618+diegobelliardo@users.noreply.github.com>
2020-12-18 09:26:00 +0100

    Terminato es 10

---
+++ 10-OOP-PersoneStudenti/10-OOP-PersoneStudenti/Studente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_OOP_PersoneStudenti
+{
+    class Studente : Persona
+    {
+        internal List<int> lstVoti = new List<int>();
+
+        public double Media()
+        {
+            int sommaVoti = 0;
+            foreach (var item in lstVoti)
+            {
+                sommaVoti += item;
+            }
+
+            return (double)sommaVoti / lstVoti.Count;
+        }
+    }
+}
