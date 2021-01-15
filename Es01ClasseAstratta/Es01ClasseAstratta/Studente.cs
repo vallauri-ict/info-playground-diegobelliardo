6d2a0d3af6407ab26f9cbff7a0bfd858e7745a49
diegobelliardo <57402618+diegobelliardo@users.noreply.github.com>
2021-01-15 09:58:48 +0100

    Terminato primo esercizio sulle classi astratte preso dalle dispense di Mana

---
+++ Es01ClasseAstratta/Es01ClasseAstratta/Studente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es01ClasseAstratta
+{
+    class Studente
+    {
+        public string Nome { get; set; }
+
+        public string Cognome { get; set; }
+
+        private DateTime dataNascita;
+
+        public Studente(string nome, string cognome)
+        {
+            Nome = nome;
+            Cognome = cognome;
+        }
+        
+        public string nomeCompleto()
+        {
+            return Nome + " " + Cognome;
+        }
+
+    }
+}
