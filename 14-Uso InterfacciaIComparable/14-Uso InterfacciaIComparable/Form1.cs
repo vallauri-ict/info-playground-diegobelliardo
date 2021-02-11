fc8f484ffe3e5215071898ec34a43f3e8f6c49b6
diegobelliardo <57402618+diegobelliardo@users.noreply.github.com>
2021-02-11 12:52:02 +0100

    Utilizzo Interfacce
    Fatti due esercizi sulle interfacce:
    1. per di prova di una prima interfaccia
    2. Implementazione di IComparable (es 14)

---
+++ 14-Uso InterfacciaIComparable/14-Uso InterfacciaIComparable/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _14_Uso_InterfacciaIComparable
+{
+    public partial class Form1 : Form
+    {
+        Persona[] vect = {new Persona("Luca","Bonelli","Fossano"),
+                          new Persona("Antonio","Ariaudo","Alba"),
+                          new Persona("Samuele","Stefirca","Savigliano")};
+        public Form1()
+        {
+            InitializeComponent();
+            ShowPersona();
+        }
+
+        public void ShowPersona()
+        {
+            string s = "";
+            foreach (var item in vect)
+            {
+                s += item.ToString() + "\n";
+            }
+            MessageBox.Show(s);
+        }
+
+        private void btnOrdina_Click(object sender, EventArgs e)
+        {
+            Array.Sort(vect);
+            ShowPersona();
+        }
+    }
+}
