371da8917f9e68416bae694c4cfd2849f8e28f3b
Diego Belliardo <diego.belliardo@vallauri.edu>
2021-05-28 10:19:51 +0200

    Terminato esercizio ML.NET Taxi L'esercizio consiste nel predirre il prezzo delle corse in taxi a New York

--- 2020-4binf/21-TaxiFarePrediction/TaxiFarePrediction/Program.cs
+++ 2020-4binf/21-TaxiFarePrediction/TaxiFarePrediction/Program.cs
@@ -1,12 +1,79 @@
-﻿using System;
+﻿using Microsoft.ML;
+using System;
+using System.IO;
 
 namespace TaxiFarePrediction
 {
     class Program
     {
+        static readonly string _trainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "taxi-fare-train.csv");
+        static readonly string _testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "taxi-fare-test.csv");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "Model.zip");
+
+        static TaxiTrip taxiTripSample = new TaxiTrip()
+        {
+            VendorId = "CMT",
+            RateCode = "1",
+            PassengerCount = 4,
+            TripTime = 903,
+            TripDistance = 3.6f,
+            PaymentType = "CSH",
+            FareAmount = 0 // To predict. Actual/Observed = 14
+        };
+
+
+        public static ITransformer Train(MLContext mlContext, string dataPath)
+        {
+            IDataView dataView = mlContext.Data.LoadFromTextFile<TaxiTrip>(dataPath, hasHeader: true, separatorChar: ',');
+            var pipeline = mlContext.Transforms.CopyColumns(outputColumnName: "Label", inputColumnName: "FareAmount")
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "VendorIdEncoded", inputColumnName: "VendorId"))
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "RateCodeEncoded", inputColumnName: "RateCode"))
+                .Append(mlContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "PaymentTypeEncoded", inputColumnName: "PaymentType"))
+                .Append(mlContext.Transforms.Concatenate("Features", "VendorIdEncoded", "RateCodeEncoded", "PassengerCount", "TripDistance", "PaymentTypeEncoded"))
+                .Append(mlContext.Regression.Trainers.FastTree());
+
+            var model = pipeline.Fit(dataView);
+
+            return model;
+        }
+
+        private static void Evaluate(MLContext mlContext, ITransformer model)
+        {
+            IDataView dataView = mlContext.Data.LoadFromTextFile<TaxiTrip>(_testDataPath, hasHeader: true, separatorChar: ',');
+            var predictions = model.Transform(dataView);
+            var metrics = mlContext.Regression.Evaluate(predictions, "Label", "Score");
+            Console.WriteLine();
+            Console.WriteLine($"*************************************************");
+            Console.WriteLine($"*       Model quality metrics evaluation         ");
+            Console.WriteLine($"*------------------------------------------------");
+            Console.WriteLine($"*       RSquared Score:      {metrics.RSquared:0.##}");
+            Console.WriteLine($"*       Root Mean Squared Error:      {metrics.RootMeanSquaredError:#.##}");
+        }
+
+        private static void TestSinglePrediction(MLContext mlContext, ITransformer model)
+        {
+            var predictionFunction = mlContext.Model.CreatePredictionEngine<TaxiTrip, TaxiTripFarePrediction>(model);
+
+            var prediction = predictionFunction.Predict(taxiTripSample);
+
+            Console.WriteLine($"**********************************************************************");
+            Console.WriteLine($"Predicted fare: {prediction.FareAmount:0.####}, actual fare: 14");
+            Console.WriteLine($"**********************************************************************");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MLContext mlContext = new MLContext(seed: 0);
+            
+            // Training del modello
+            var model = Train(mlContext, _trainDataPath);
+
+            //valutazione del modello
+            Evaluate(mlContext, model);
+
+            //Uso del modello per stimare una corsa
+            TestSinglePrediction(mlContext, model);
+
         }
     }
 }
