fc13ca0df7928c8dfe2f6533bf7c6894d5c1a3c4
Prof. Diego Belliardo <diego.belliardo@vallauri.edu>
2021-09-17 13:06:43 +0200

    Console e DB iniziato
    Creato il progetto console e realizzato il DB con primo test di accesso ai dati

---
+++ 2021-5binf/OnlineRegisterSolution/Data/Student.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Student]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] VARCHAR(20) NOT NULL, 
+    [LastName] VARCHAR(30) NOT NULL
+)
--- 2021-5binf/OnlineRegisterSolution/OnlineRegisterConsole/Program.cs
+++ 2021-5binf/OnlineRegisterSolution/OnlineRegisterConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace OnlineRegisterConsole
 {
@@ -6,7 +7,68 @@ namespace OnlineRegisterConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title="Online Register";
+            char choise = ' ';
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("\n*** ONLINE REGISTER BY VALLAURI ***\n");
+                Console.WriteLine("1 - Student list");
+                Console.WriteLine("2 - Class list");
+                Console.WriteLine("3 -");
+                Console.WriteLine("4 -");
+                Console.WriteLine("5 -");
+                Console.WriteLine("-------------------");
+                Console.WriteLine("B - Backup Data");
+                Console.WriteLine("T - Restore Data");
+                Console.WriteLine("R - Reset Data");
+                Console.WriteLine("-------------------");
+                Console.WriteLine("X - Exit\n");
+
+                choise = Console.ReadKey(true).KeyChar;
+
+                switch (choise)
+                {
+                    case '1':
+                        
+                        StudentList();
+                        Console.ReadKey(true);
+                        break;
+                    case '2':
+                        Console.WriteLine("Call class list");
+                        Console.ReadKey(true);
+                        break;
+                    default:
+                        Console.WriteLine("Work in progress");
+                        Console.ReadKey(true);
+                        break;
+                }
+
+            } while (choise !='x' && choise !='X');
+        }
+
+        public static void StudentList()
+        {
+            string connStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\fenic\Desktop\info-playground-diegobelliardo\2021-5binf\OnlineRegisterSolution\Data\OnlineRegister\Register.mdf;Integrated Security=True;Connect Timeout=30";
+            using (SqlConnection dbConn = new SqlConnection(connStr))
+            {
+                using (SqlCommand dbCmd=new SqlCommand("SELECT * FROM Student",dbConn))
+                {
+                    dbConn.Open();
+                    using (SqlDataReader dbReader = dbCmd.ExecuteReader())
+                    {
+                        while (dbReader.Read())
+                        {
+                            Console.WriteLine(dbReader.GetString(1)+" - "+dbReader.GetString(2));
+                        }
+                    }
+                }
+                    
+
+
+
+            }
         }
     }
 }
