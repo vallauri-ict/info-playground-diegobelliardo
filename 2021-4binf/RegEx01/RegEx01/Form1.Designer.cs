4616ff9d1440d6e48e5dae0da9af7f4e44a204ab
Diego Belliardo <diego.belliardo@vallauri.edu>
2021-12-04 11:08:53 +0100

    Iniziato es su Regex --DA FINIRE --

---
+++ 2021-4binf/RegEx01/RegEx01/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace RegEx01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStr = new System.Windows.Forms.TextBox();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnMatch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inserisci la stringa da controllare";
+            // 
+            // txtStr
+            // 
+            this.txtStr.Location = new System.Drawing.Point(176, 10);
+            this.txtStr.Name = "txtStr";
+            this.txtStr.Size = new System.Drawing.Size(203, 20);
+            this.txtStr.TabIndex = 1;
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(110, 48);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 2;
+            this.btnTest.Text = "Verifica";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(410, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(359, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Uso di Match nella stringa:  \"One car red car blue car\"";
+            // 
+            // btnMatch
+            // 
+            this.btnMatch.Location = new System.Drawing.Point(548, 48);
+            this.btnMatch.Name = "btnMatch";
+            this.btnMatch.Size = new System.Drawing.Size(75, 23);
+            this.btnMatch.TabIndex = 4;
+            this.btnMatch.Text = "Test Stringa";
+            this.btnMatch.UseVisualStyleBackColor = true;
+            this.btnMatch.Click += new System.EventHandler(this.btnMatch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(825, 99);
+            this.Controls.Add(this.btnMatch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.txtStr);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStr;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnMatch;
+    }
+}
+
---
+++ 2021-4binf/RegEx01/RegEx01/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RegEx01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            string pattern = @"^[a-zA-Z0-9]\d{2}[a-zA-Z0-9](-\d{3}){2}[A-Za-z0-9]$";
+            Regex rgx = new Regex(pattern);
+            string str = txtStr.Text;
+
+            MessageBox.Show(str+"  "+rgx.IsMatch(str));
+
+            MessageBox.Show(str + "  " +Regex.IsMatch(str,pattern));
+        }
+
+        private void btnMatch_Click(object sender, EventArgs e)
+        {
+            string str = "One car red car blue car";
+            string pattern= @"(\w+)\s+(car)";
+            int count = 0;
+
+            Regex reg = new Regex(pattern);
+            Match m = reg.Match(str);
+            
+            while (m.Success)
+            {
+                MessageBox.Show("Trovato: gruppo "+(++count)+" Valore: "+m.Value+
+                                " Alla posizione: "+m.Index);
+
+                m = m.NextMatch();
+            }
+        }
+    }
+}
