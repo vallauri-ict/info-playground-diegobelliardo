28071dd6054808531f6b3f8e7c30126ffff6b1c4
Diego Belliardo <diego.belliardo@vallauri.net>
2022-01-22 13:12:34 +0100

    Es SQL prova utilizzo SQL INNER LEFT RIGHT

---
+++ 2021-5binf/EsSQL05-Join/SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿SELECT *
+FROM Continenti c, Nazioni n
+WHERE c.Id=n.codContinente
+
+SELECT *
+FROM Continenti c INNER JOIN Nazioni n ON c.Id=n.codContinente
+
+SELECT *
+FROM Continenti c LEFT JOIN Nazioni n ON  c.Id=n.codContinente
