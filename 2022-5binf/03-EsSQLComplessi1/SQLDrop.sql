156977969e0e56bc1a4aa7b5c4a44293316addb5
Diego Belliardo <diego.belliardo@vallauri.edu>
2022-11-26 13:21:31 +0100

    SQLComplesso prime query

---
+++ 2022-5binf/03-EsSQLComplessi1/SQLDrop.sql
@@ -0,0 +1,5 @@
+﻿drop table Proiezioni
+drop table Recita
+drop table Attori
+drop table Film
+drop table Sale
---
+++ 2022-5binf/03-EsSQLComplessi1/SQLQuery.sql
@@ -0,0 +1,29 @@
+﻿-- 1- Il nome di tutte le sale di Pisa
+
+SELECT s.Nome
+FROM Sale s
+WHERE s.Citta='Fossano'
+
+-- 2- Il titolo dei film di F. Fellini prodotti dopo il 1960.
+
+SELECT f.Titolo
+FROM Film f
+WHERE f.AnnoProduzione>1960
+AND f.Regista='Pietro Valsecchi'
+
+-- 3- Il titolo e la durata dei film di fantascienza giapponesi o francesi prodotti dopo il 1990
+
+SELECT f.Titolo
+FROM Film f
+WHERE f.Genere='Fantascienza'
+AND (f.Nazionalita='Giapponese' OR f.Nazionalita='Francese')
+AND f.AnnoProduzione>1990
+
+-- 4- Il titolo dei film di fantascienza giapponesi prodotti dopo il 1990 oppure francesi
+
+SELECT f.Titolo
+FROM Film f
+WHERE f.Genere='Fantascienza'
+AND (f.Nazionalita='Giapponese' 
+AND f.AnnoProduzione>1990)
+OR f.Nazionalita='Francese'
