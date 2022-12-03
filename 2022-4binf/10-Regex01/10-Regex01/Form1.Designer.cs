8e23196b4ee4c8db7a811d1c455227332833e121
Diego Belliardo <diego.belliardo@vallauri.edu>
2022-12-03 09:49:08 +0100

    Regex01

---
+++ 2022-4binf/10-Regex01/10-Regex01/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace _10_Regex01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtStr = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCarta = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtStr
+            // 
+            this.txtStr.Location = new System.Drawing.Point(178, 23);
+            this.txtStr.Name = "txtStr";
+            this.txtStr.Size = new System.Drawing.Size(176, 20);
+            this.txtStr.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Inserisci la stringa da controllare:";
+            // 
+            // btnCarta
+            // 
+            this.btnCarta.Location = new System.Drawing.Point(83, 73);
+            this.btnCarta.Name = "btnCarta";
+            this.btnCarta.Size = new System.Drawing.Size(143, 23);
+            this.btnCarta.TabIndex = 2;
+            this.btnCarta.Text = "Testa Numero Carta";
+            this.btnCarta.UseVisualStyleBackColor = true;
+            this.btnCarta.Click += new System.EventHandler(this.btnCarta_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCarta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStr);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtStr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCarta;
+    }
+}
+
---
+++ 2022-4binf/10-Regex01/10-Regex01/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10_Regex01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCarta_Click(object sender, EventArgs e)
+        {
+            Regex reg=new Regex(@"^[a-zA-Z0-9]\d{2}[a-zA-Z0-9](-\d{3}){2}[A-Za-z0-9]$");
+
+            if (reg.IsMatch(txtStr.Text))
+            {
+                MessageBox.Show("La stringa "+ txtStr.Text+" è corretta");
+            }
+            else MessageBox.Show("La stringa " + txtStr.Text + " NON è corretta");
+        }
+    }
+}
