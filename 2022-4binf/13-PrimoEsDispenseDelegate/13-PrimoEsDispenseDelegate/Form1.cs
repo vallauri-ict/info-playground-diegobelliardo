bb34612ff0a61ccaf75a818e7a6648791fcb8555
Diego Belliardo <diego.belliardo@vallauri.edu>
2023-01-25 12:27:41 +0100

    Esempio 1 (gestione eventi) dispense prof. Mana

---
+++ 2022-4binf/13-PrimoEsDispenseDelegate/13-PrimoEsDispenseDelegate/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13_PrimoEsDispenseDelegate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalva_Click(object sender, EventArgs e)
+        {
+            Fattura f = new Fattura();
+
+            f.salvataggioEseguito += MiaFunzione;
+            f.salvataggioEseguito += new salvataggioEseguitoEventHandler(MiaFunzione2);
+
+            f.SalvaSuFile();
+        }
+
+        private void MiaFunzione2(int id)
+        {
+            MessageBox.Show("Secondo gestore di evento!");
+        }
+
+        private void MiaFunzione(int id)
+        {
+            MessageBox.Show("Salvataggio Fattura "+id+" AVVENTO!");
+        }
+    }
+}
