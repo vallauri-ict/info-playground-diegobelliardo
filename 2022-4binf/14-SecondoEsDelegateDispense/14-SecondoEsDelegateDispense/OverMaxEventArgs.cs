418fecfea888dc851efccde8891d8d5478e5cca0
Diego Belliardo <diego.belliardo@vallauri.edu>
2023-01-25 13:12:17 +0100

    Esempio 2 (gestione eventi) dispense prof. Mana

---
+++ 2022-4binf/14-SecondoEsDelegateDispense/14-SecondoEsDelegateDispense/OverMaxEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_SecondoEsDelegateDispense
+{
+    public class OverMaxEventArgs : EventArgs
+    {
+        public int ValoreSoglia { get; }
+
+        public OverMaxEventArgs(int valoreSoglia)
+        {
+            ValoreSoglia = valoreSoglia;
+        }
+    }
+}
