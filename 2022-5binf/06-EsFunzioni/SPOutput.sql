a4717bc4fdfea484e4894b0869acd993ae8e88d8
Diego Belliardo <diego.belliardo@vallauri.net>
2023-01-31 13:21:44 +0100

    Funzioni e StoredProcedure

---
+++ 2022-5binf/06-EsFunzioni/SPOutput.sql
@@ -0,0 +1,6 @@
+﻿DECLARE @fullNameRes varchar(100)
+DECLARE @countryRes char(2)
+EXEC dbo.SPInfoPilota  @number=1, @fullName=@fullNameRes output, 
+								  @country=@countryRes output;
+
+SELECT @fullNameRes,@countryRes
