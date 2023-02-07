434099a4e6156619bcd3d1b319b68ae8d40a74e8
Diego Belliardo <diego.belliardo@vallauri.net>
2023-02-07 12:50:38 +0100

    Prima parte Form TriggerTest query delete

---
+++ 2022-5binf/07-EsTriggerForm/07-EsTriggerForm/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace _07_EsTriggerForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDriver = new System.Windows.Forms.DataGridView();
+            this.dgvStoricoCancellazioni = new System.Windows.Forms.DataGridView();
+            this.dgvStoricoUpdate = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDriver)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStoricoCancellazioni)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStoricoUpdate)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDriver
+            // 
+            this.dgvDriver.AllowUserToAddRows = false;
+            this.dgvDriver.AllowUserToDeleteRows = false;
+            this.dgvDriver.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDriver.Location = new System.Drawing.Point(14, 13);
+            this.dgvDriver.Name = "dgvDriver";
+            this.dgvDriver.RowHeadersVisible = false;
+            this.dgvDriver.Size = new System.Drawing.Size(488, 215);
+            this.dgvDriver.TabIndex = 0;
+            this.dgvDriver.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDriver_CellDoubleClick);
+            // 
+            // dgvStoricoCancellazioni
+            // 
+            this.dgvStoricoCancellazioni.AllowUserToAddRows = false;
+            this.dgvStoricoCancellazioni.AllowUserToDeleteRows = false;
+            this.dgvStoricoCancellazioni.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStoricoCancellazioni.Location = new System.Drawing.Point(508, 12);
+            this.dgvStoricoCancellazioni.Name = "dgvStoricoCancellazioni";
+            this.dgvStoricoCancellazioni.RowHeadersVisible = false;
+            this.dgvStoricoCancellazioni.Size = new System.Drawing.Size(516, 215);
+            this.dgvStoricoCancellazioni.TabIndex = 1;
+            // 
+            // dgvStoricoUpdate
+            // 
+            this.dgvStoricoUpdate.AllowUserToAddRows = false;
+            this.dgvStoricoUpdate.AllowUserToDeleteRows = false;
+            this.dgvStoricoUpdate.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStoricoUpdate.Location = new System.Drawing.Point(14, 235);
+            this.dgvStoricoUpdate.Name = "dgvStoricoUpdate";
+            this.dgvStoricoUpdate.RowHeadersVisible = false;
+            this.dgvStoricoUpdate.Size = new System.Drawing.Size(1010, 203);
+            this.dgvStoricoUpdate.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1036, 450);
+            this.Controls.Add(this.dgvStoricoUpdate);
+            this.Controls.Add(this.dgvStoricoCancellazioni);
+            this.Controls.Add(this.dgvDriver);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDriver)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStoricoCancellazioni)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStoricoUpdate)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDriver;
+        private System.Windows.Forms.DataGridView dgvStoricoCancellazioni;
+        private System.Windows.Forms.DataGridView dgvStoricoUpdate;
+    }
+}
+
---
+++ 2022-5binf/07-EsTriggerForm/07-EsTriggerForm/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07_EsTriggerForm
+{
+    public partial class Form1 : Form
+    {
+        public static readonly string workingDirectory = Environment.CurrentDirectory;
+        public static readonly string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+        public static readonly string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + projectDirectory + @"\Piloti.mdf;Integrated Security=True;Connect Timeout=30";
+
+        private BindingSource bindingSourceDriver = new BindingSource();
+        private BindingSource bindingSourceStoricoCanc = new BindingSource();
+        private BindingSource bindingSourceStoricoUpd = new BindingSource();
+        private SqlDataAdapter dataAdapterDriver, dataAdapterStorico;
+        private DataTable dtDriver, dtStorico;
+
+        public Form1()
+        {
+            InitializeComponent();
+//            MessageBox.Show("Stringa di connessione: "+CONNECTION_STRING);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopolaDgv();
+        }
+
+        private void PopolaDgv()
+        {
+            DataTable table;
+
+            //******* DRIVER ********/
+            dgvDriver.DataSource = bindingSourceDriver;
+            QueryDriver("SELECT * FROM Driver");
+            bindingSourceDriver.DataSource = dtDriver;
+
+            //Resize del DGV
+            dgvDriver.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+
+            //******* STORICO CANCELLAZIONI ********/
+            dgvStoricoCancellazioni.DataSource=bindingSourceStoricoCanc;
+            QueryStorico("SELECT * FROM StoricoCancellazioni");
+            bindingSourceStoricoCanc.DataSource = dtStorico;
+
+            //Resize
+            dgvStoricoCancellazioni.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+
+            //******* STORICO UPDATE ********/
+            dgvStoricoUpdate.DataSource = bindingSourceStoricoUpd;
+//            QueryUpdate("SELECT * FROM StoricoAggiornamenti");
+
+        }
+
+        private void QueryUpdate(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void dgvDriver_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string pk = dgvDriver.Rows[e.RowIndex].Cells["number"].Value.ToString();
+            MessageBox.Show(pk);
+            string delete = "DELETE FROM Driver WHERE number=" + pk;
+            QueryDriver(delete);
+            PopolaDgv();
+        }
+
+        private void QueryStorico(string sqlCommand)
+        {
+            try
+            {
+                // Creiamo un data adapter basato su una query
+                dataAdapterStorico = new SqlDataAdapter(sqlCommand, CONNECTION_STRING);
+
+                // Creiamo un command builder per generare SQL di update, inser, delete
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapterStorico);
+
+                //Popoliamo il data Table associato al BindingSource
+                dtStorico = new DataTable();
+                dataAdapterStorico.Fill(dtStorico);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void QueryDriver(string sqlCommand)
+        {
+            try
+            {
+                // Creiamo un data adapter basato su una query
+                dataAdapterDriver = new SqlDataAdapter(sqlCommand,CONNECTION_STRING);
+
+                // Creiamo un command builder per generare SQL di update, inser, delete
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapterDriver);
+
+                //Popoliamo il data Table associato al BindingSource
+                dtDriver = new DataTable();
+                dataAdapterDriver.Fill(dtDriver);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
