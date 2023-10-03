5061de35507e23dec8832339d852f8e6813b202f
Diego Belliardo <diego.belliardo@vallauri.edu>
2023-10-03 09:43:55 +0200

    Correzione verifica SQL

--- 2023-5binf/03-EsSQL per verifica/SQLQuery2.sql
+++ 2023-5binf/03-EsSQL per verifica/SQLQuery2.sql
@@ -40,3 +40,45 @@ SELECT f.Titolo, f.Genere
 FROM Film f, Proiezioni p
 WHERE f.Id=p.CodFilm
 AND p.DataProiezione='2004/12/25'
+
+-- 7. Il titolo ed il genere dei film proiettati a Napoli il giorno di Natale 2004
+SELECT f.Titolo, f.Genere
+FROM Film f, Proiezioni p, Sale s
+WHERE f.Id = p.CodFilm
+AND s.Id = p.codSala
+AND s.Città = 'Napoli'
+AND p.DataProiezione = '2004/12/25'; -- Oppure p.DataProiezione = '20041225'
+
+-- 8. I nomi delle sale di Napoli in cui il giorno di Natale 2004 è stato proiettato un film con R.Williams
+SELECT DISTINCT s.Nome
+FROM Sale s, Proiezioni p, Attori a, Recita r
+WHERE s.Città = 'Napoli'
+AND p.DataProiezione = '2004/12/25'
+AND p.CodSala = s.Id
+AND p.CodFilm = r.CodFilm
+AND r.CodAttore = a.Id
+AND a.Nome = 'Williams';
+
+-- 9. Il titolo dei film in cui recita M. Mastroianni oppure S.Loren
+SELECT DISTINCT f.Titolo
+FROM Film f, Attori a, Recita r
+WHERE f.Id = r.CodFilm
+AND a.Id = r.CodAttore
+AND (a.Nome = 'Mastroianni' OR a.Nome = 'Loren');
+
+-- 10. Il titolo dei film in cui recitano M. Mastroianni e S.Loren
+SELECT f.Titolo
+FROM Film f
+WHERE 'Mastroianni' IN (SELECT a.Nome FROM Attori a, Recita r WHERE r.CodFilm = f.Id AND r.CodAttore = a.Id)
+AND 'Loren' IN (SELECT a.Nome FROM Attori a, Recita r WHERE r.CodFilm = f.Id AND r.CodAttore = a.Id);
+
+-- 13. Il numero di sale di Pisa con più di 60 posti
+SELECT COUNT(*) AS NumeroSale
+FROM Sale s
+WHERE s.Città = 'Pisa'
+AND s.Posti > 60;
+
+-- 14. Il numero totale di posti nelle sale di Pisa
+SELECT SUM(s.Posti) AS NumeroPostiTotale
+FROM Sale s
+WHERE s.Città = 'Pisa';
---
+++ 2023-5binf/04-EsSQL Correzione verifica/SQLQuery1.sql
@@ -0,0 +1,97 @@
+﻿-- Romanzi pubblicati a partire dal 1990
+SELECT *
+from Romanzo r
+WHERE r.AnnoPubblicazione >= 1990
+
+-- Romanzi del ventesimo secolo
+SELECT *
+FROM Romanzo r
+WHERE r.AnnoPubblicazione >= 1901
+AND r.AnnoPubblicazione <= 2000
+
+SELECT *
+FROM Romanzo r
+WHERE r.AnnoPubblicazione BETWEEN 1901 and 2000
+
+-- Autore (cognome e nome) e titolo di tutti i romanzi, ordinati per 
+-- cognome e nome dell'autore
+SELECT a.Cognome, a.Nome, r.Titolo
+FROM Autore a, Romanzo r
+WHERE r.IdAutore=a.Id
+ORDER BY a.Cognome, a.Nome
+
+-- Autore (cognome e nome) e titolo di tutti i romanzi russi,
+-- ordinati per cognome e nome dell'autore
+SELECT a.Cognome, a.Nome, r.Titolo
+FROM Autore a, Romanzo r
+WHERE r.IdAutore=a.Id
+AND a.Nazionalità='Russa'
+ORDER BY a.Cognome, a.Nome
+
+-- Titolo dei romanzi di autori nati nel ventesimo secolo
+-- ordinati per cognome e nome dell'autore e 
+-- data di pubblicazione
+SELECT *
+FROM Autore a, Romanzo r
+WHERE r.IdAutore=a.Id
+AND a.DataNascita>='1901/01/01'
+AND a.DataNascita<='2000/12/31'
+
+SELECT *
+FROM Autore a, Romanzo r
+WHERE r.IdAutore=a.Id
+AND YEAR(a.DataNascita) >= 1901
+AND YEAR(a.DataNascita) <= 2000
+
+--Titolo e anno pubblicazione romanzi di autori viventi
+SELECT r.Titolo, r.AnnoPubblicazione
+FROM Autore a, Romanzo r
+WHERE r.IdAutore=a.Id
+AND a.DataMorte is null
+
+-- Romanzi di scrittori viventi, oppure morti ma non a Torino
+SELECT r.Titolo, r.AnnoPubblicazione
+FROM Autore a, Romanzo r
+WHERE r.IdAutore=a.Id
+AND (a.DataMorte is null
+OR a.LuogoMorte <> 'Torino')
+
+-- Romanzi con la parola sposi nel titolo
+SELECT r.Titolo
+FROM Romanzo r
+WHERE r.Titolo like '%sposi%'
+
+-- Case sensitive insensitive
+SELECT SERVERPROPERTY('COLLATION')
+
+SELECT name, description FROM sys.fn_helpcollations()
+WHERE name like '%Lati%'
+
+ALTER TABLE Romanzo
+ALTER COLUMN Titolo VARCHAR(50)
+COLLATE Latin1_General_CS_AS
+
+-- Romanzi pubblicati prima del 1990 
+-- con la parola sposi nel titolo 
+-- scritta con l'iniziale maiuscola o minuscola
+SELECT r.Titolo
+FROM Romanzo r
+WHERE (r.Titolo like '%sposi%'
+OR r.Titolo like '%Sposi%')
+AND r.AnnoPubblicazione < 1990
+
+-- Titolo dei romanzi di Don Delillo escludendo i duplicati
+SELECT distinct r.Titolo
+FROM Autore a, Romanzo r
+WHERE r.IdAutore=a.Id
+AND a.Cognome='Delillo'
+and a.Nome='Don'
+
+-- Romanzi con il personaggio Benjamin
+SELECT r.Titolo, p.*
+FROM Personaggio p, PersonaggioRomanzo pr,
+	Romanzo r
+WHERE p.Id=pr.IdPersonaggio
+AND pr.IdRomanzo=r.Id
+AND p.Nome='Benjamin'
+AND p.Cognome='Malaussène'
