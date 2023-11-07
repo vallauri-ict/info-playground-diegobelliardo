0638d385388197a95a2aac76b07252458192e669
Diego Belliardo <diego.belliardo@vallauri.edu>
2023-11-07 09:46:18 +0100

    Es SQL Scuola

---
+++ 2023-5binf/07-EsSQL Scuola/SQLQuery2.sql
@@ -0,0 +1,20 @@
+﻿ALTER TABLE CorsiDiLaurea
+ADD CONSTRAINT PK_CorsiDiLaurea PRIMARY KEY (CorsoLaurea);
+
+ALTER TABLE Persons
+ADD CONSTRAINT PK_Person PRIMARY KEY (ID,LastName);
+
+
+ALTER TABLE Studenti 
+ADD CONSTRAINT StudentiCorsolaurea FOREIGN KEY (CorsoLaurea) REFERENCES CORSIDILAUREA(CorsoLaurea)
+
+
+
+ALTER TABLE CorsiDiLaurea
+ALTER COLUMN CorsoLaurea VARCHAR(50) NOT NULL;
+
+
+select name 
+   from sysobjects 
+  where xtype = 'PK' 
+    
---
+++ 2023-5binf/07-EsSQL Scuola/dbo.Docenti.data.sql
@@ -0,0 +1,1 @@
+﻿INSERT INTO [dbo].[Docenti] ([CodDocente], [NomeD], [Dipartimento]) VALUES (1, N'Belliardo', N'Basi di dati e sistemi informativi')
