9d92c52b2a797dde44c3bc6d4c0736c8dc58e53d
Prof. Diego Belliardo <diego.belliardo@vallauri.edu>
2023-11-14 11:42:11 +0100

    Inizio correzione verifica

---
+++ 2023-5binf/08-EsSQL Verifica/SQLQuery1.sql
@@ -0,0 +1,16 @@
+﻿SELECT Categorie.NomeCategoria,
+       SUM(CASE WHEN Libri.Disponibilita = 0 THEN 1 ELSE 0 END) * 100.0 / COUNT(Libri.IDLibro) AS PercentualeNonDisponibili
+FROM Categorie
+JOIN Libri ON Categorie.IDCategoria = Libri.CategoriaID
+GROUP BY Categorie.NomeCategoria
+ORDER BY PercentualeNonDisponibili DESC;
+
+
+SELECT c.NomeCategoria, count(*)/(SELECT COUNT(*) FROM Libri l1, Categorie c1 
+                                    Where l1.CategoriaID=c1.IDCategoria AND c1.NomeCategoria=c.NomeCategoria)
+FROM Categorie c, Libri l
+Where l.CategoriaID=c.IDCategoria
+AND l.Disponibilita=0
+GROUP BY c.NomeCategoria
+order by count(*) DESC
+
