41b8e92c572f154743f9a5068a54b254b5fcc474
Prof. Diego Belliardo <diego.belliardo@vallauri.edu>
2023-11-17 09:56:44 +0100

    SQL Verifica

--- 2023-5binf/08-EsSQL Verifica/SQLQuery1.sql
+++ 2023-5binf/08-EsSQL Verifica/SQLQuery1.sql
@@ -5,12 +5,21 @@ JOIN Libri ON Categorie.IDCategoria = Libri.CategoriaID
 GROUP BY Categorie.NomeCategoria
 ORDER BY PercentualeNonDisponibili DESC;
 
-
-SELECT c.NomeCategoria, count(*)/(SELECT COUNT(*) FROM Libri l1, Categorie c1 
-                                    Where l1.CategoriaID=c1.IDCategoria AND c1.NomeCategoria=c.NomeCategoria)
+SELECT TOP 1 c.NomeCategoria, count(*)*100.0/ (SELECT COUNT(*) FROM Libri l1, Categorie c1 
+                                    Where l1.CategoriaID=c1.IDCategoria AND c1.NomeCategoria=c.NomeCategoria) as percentuale
 FROM Categorie c, Libri l
 Where l.CategoriaID=c.IDCategoria
 AND l.Disponibilita=0
 GROUP BY c.NomeCategoria
-order by count(*) DESC
+order by percentuale DESC
 
+SELECT  NomeAutore, CognomeAutore, COUNT(Libri.IDLibro)
+FROM Autori
+JOIN Libri ON Autori.IDAutore = Libri.AutoreID
+GROUP BY NomeAutore, CognomeAutore
+ORDER BY COUNT(Libri.IDLibro) DESC;
+
+SELECT  NomeAutore, CognomeAutore
+FROM Autori a
+GROUP BY NomeAutore, CognomeAutore
+HAVING MAX(COUNT(*)) >(SELECT Count(*) FROM Libri l WHERE l.AutoreID= a.IDAutore)
