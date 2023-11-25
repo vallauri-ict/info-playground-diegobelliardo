4bf82f5ab6c27b7d971e081e6ed03f63b393c7e4
Diego <diego.belliardo@vallauri.edu>
2023-11-25 13:29:02 +0100

    Correzione es SQLVerifica

--- 2023-5binf/08-EsSQL Verifica/SQLQuery1.sql
+++ 2023-5binf/08-EsSQL Verifica/SQLQuery1.sql
@@ -1,4 +1,37 @@
-﻿SELECT Categorie.NomeCategoria,
+﻿--Query 1 - Visualizza tutti i libri con il loro titolo, autore e categoria.
+
+SELECT Libri.Titolo, Autori.NomeAutore, Categorie.NomeCategoria
+FROM Libri
+JOIN Autori ON Libri.AutoreID = Autori.IDAutore
+JOIN Categorie ON Libri.CategoriaID = Categorie.IDCategoria;
+
+--Query 2 - Trova il nome dell'autore che ha scritto più libri.
+
+SELECT TOP 1 NomeAutore, CognomeAutore
+FROM Autori
+JOIN Libri ON Autori.IDAutore = Libri.AutoreID
+GROUP BY NomeAutore, CognomeAutore
+ORDER BY COUNT(Libri.IDLibro) DESC;
+
+--Query 3 - Mostra il titolo e l'autore dei libri non disponibili al momento
+
+SELECT Libri.Titolo, Autori.NomeAutore
+FROM Libri
+JOIN Autori ON Libri.AutoreID = Autori.IDAutore
+WHERE Libri.Disponibilita = 0;	
+
+--Query 4 - Conta il numero di prestiti effettuati da ciascun utente e visualizza il risultato in ordine decrescente.
+
+SELECT IDUtente, COUNT(IDPrestito) AS NumeroPrestiti
+FROM Prestiti
+GROUP BY IDUtente
+ORDER BY NumeroPrestiti DESC;
+
+--Query 5 - Trova la categoria con la più alta percentuale di libri non disponibili 
+-- al momento. Mostra il nome della categoria e la percentuale di libri 
+-- non disponibili rispetto al totale dei libri in quella categoria.
+
+SELECT Categorie.NomeCategoria,
        SUM(CASE WHEN Libri.Disponibilita = 0 THEN 1 ELSE 0 END) * 100.0 / COUNT(Libri.IDLibro) AS PercentualeNonDisponibili
 FROM Categorie
 JOIN Libri ON Categorie.IDCategoria = Libri.CategoriaID
@@ -12,6 +45,34 @@ Where l.CategoriaID=c.IDCategoria
 AND l.Disponibilita=0
 GROUP BY c.NomeCategoria
 order by percentuale DESC
+
+
+
+--Query 6 - Restituisci i primi 5 autori con la media di libri pubblicati 
+-- per categoria di libri. Considera solo le categorie con almeno 3 libri.
+
+WITH MediaLibriPerCategoria AS (
+    SELECT Categorie.NomeCategoria, AVG(Disponibilita) AS MediaLibri
+    FROM Categorie
+    JOIN Libri ON Categorie.IDCategoria = Libri.CategoriaID
+    GROUP BY Categorie.NomeCategoria
+    HAVING COUNT(Libri.IDLibro) >= 3
+)
+SELECT TOP 5 Autori.NomeAutore, Autori.CognomeAutore, MediaLibriPerCategoria.NomeCategoria, MediaLibri
+FROM Autori
+JOIN Libri ON Autori.IDAutore = Libri.AutoreID
+JOIN Categorie ON Libri.CategoriaID = Categorie.IDCategoria
+JOIN MediaLibriPerCategoria ON Categorie.NomeCategoria = MediaLibriPerCategoria.NomeCategoria
+ORDER BY MediaLibri DESC;
+
+
+
+
+
+
+
+
+
 
 SELECT  NomeAutore, CognomeAutore, COUNT(Libri.IDLibro)
 FROM Autori
