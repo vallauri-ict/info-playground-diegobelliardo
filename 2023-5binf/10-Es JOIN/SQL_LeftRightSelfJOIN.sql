85be8f115e35e1b3d5f756f08089f65a63d6a92e
Diego Belliardo <diego.belliardo@vallauri.net>
2024-02-20 09:18:00 +0100

    Left Right Self JOIN e Union Intersect Except

---
+++ 2023-5binf/10-Es JOIN/SQL_LeftRightSelfJOIN.sql
@@ -0,0 +1,18 @@
+﻿select *
+from Studenti s, Voti v
+WHERE s.matricola=v.matricola
+
+select *
+from Studenti s LEFT JOIN Voti v ON s.matricola=v.matricola
+
+select *
+from Voti v RIGHT JOIN Studenti s ON s.matricola=v.matricola
+
+select *
+from Studenti s LEFT JOIN Voti v ON s.matricola=v.matricola
+where v.matricola is null
+
+select g1.genitore AS Nonni
+from Genitori g1, Genitori g2
+where g1.figlio=g2.genitore
+AND g2.figlio='Anna'
---
+++ 2023-5binf/10-Es JOIN/sql_UnionIntersectExcept.sql
@@ -0,0 +1,24 @@
+﻿select v.data
+from VenditeNegozio v
+UNION
+select v.data
+from VenditeInternet v
+
+select v.data
+from VenditeInternet v
+UNION
+select v.data
+from VenditeNegozio v
+order by v.data desc
+
+select v.data
+from VenditeNegozio v
+INTERSECT
+select v.data
+from VenditeInternet v
+
+select N.data
+from VenditeNegozio N
+except
+select I.data
+from VenditeInternet I
