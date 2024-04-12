615ceb50a769c090f9c417c49e003d6e88a8bdaf
Diego Belliardo <diego.belliardo@vallauri.edu>
2024-04-12 08:51:20 +0200

    Trigger01

---
+++ 2023-5binf/11-EsTrigger01/dbo.Trigger.sql
@@ -0,0 +1,32 @@
+﻿CREATE TRIGGER [NuovaAuto]
+	ON Auto
+	INSTEAD OF INSERT
+	AS
+	BEGIN
+--		SET NOCOUNT ON
+		DECLARE @targa as VARCHAR(7)
+		DECLARE @cilindrata as INT
+		DECLARE @prezzo as float
+		DECLARE @modello as VARCHAR(50)
+		DECLARE @colore as VARCHAR(50)
+		DECLARE @marca as VARCHAR(50)
+		DECLARE @alimentazione as VARCHAR(50)
+
+		SET @targa = (SELECT targa from inserted)
+		SET @cilindrata = (SELECT cilindrata from inserted)
+		SET @prezzo = (SELECT prezzo from inserted)
+		SET @modello = (SELECT modello from inserted)
+		SET @colore = (SELECT colore from inserted)
+		SET @marca = (SELECT marca from inserted)
+		SET @alimentazione = (SELECT alimentazione from inserted)
+
+		if (@cilindrata<900)
+			SET @cilindrata = 900
+
+		if (@cilindrata>4800)
+			SET @cilindrata = 4800
+
+		INSERT INTO Auto Values
+		(@targa,@cilindrata,@prezzo,@modello,@colore,@marca,@alimentazione)
+
+	END
