363666fe099008df47225110b6564fedab295424
Diego Belliardo <diego.belliardo@vallauri.edu>
2024-04-17 08:50:03 +0200

    Continuazione SQL

--- 2023-4binf/21-SQL01Auto/SQLQuery1.sql
+++ 2023-4binf/21-SQL01Auto/SQLQuery1.sql
@@ -1,7 +1,16 @@
 ﻿SELECT *
 FROM Auto a
+Where a.targa = 'AA001AA'
+
+
+SELECT *
+FROM Auto a
 Where a.targa LIKE '_B%'
 
 SELECT *
 FROM Auto a
 Where a.prezzo BETWEEN 10000 and 14500
+
+SELECT *
+FROM Auto a
+WHERE colore IN ('rosso','grigio')
