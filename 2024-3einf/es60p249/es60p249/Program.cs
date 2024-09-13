d2f5995c9374f8e9ce4d25c114d27df28cfbecb8
Diego Belliardo <diego.belliardo@vallauri.net>
2024-09-13 10:43:16 +0200

    Iniziato esercizio 3E

---
+++ 2024-3einf/es60p249/es60p249/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es60p249
+{
+    class Program
+    {
+        /* Ricevuto in ingresso un numero intero positivo n, visualizza un triangolo formato da n righe.
+        Per esempio, se n=4 visualizzi:
+        1
+        2 3
+        4 5 6
+        7 8 9 10*/
+
+        // linea singola da commentare
+
+        static void Main(string[] args)
+        {
+            int n=1, i, j, cont = 1;
+
+            do
+            {
+                Console.WriteLine("inserisci un numero intero positivo:\n");
+                Console.ReadKey();
+
+            } while (n <= 0);
+
+
+        }
+    }
+}
