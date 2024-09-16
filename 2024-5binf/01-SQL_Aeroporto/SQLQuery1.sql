383673d0ab1aaea568823d3296c44fac628743f1
Diego Belliardo <diego.belliardo@vallauri.edu>
2024-09-16 08:51:23 +0200

    Primo es SQL 5B

---
+++ 2024-5binf/01-SQL_Aeroporto/SQLQuery1.sql
@@ -0,0 +1,20 @@
+﻿/*  Trovare le città da cui partono voli diretti a Roma, ordinate alfabeticamente- 
+*/
+
+SELECT v.cittaPartenza
+FROM volo v
+WHERE v.cittaArrivo='Roma'
+ORDER BY v.cittaPartenza 
+
+/* Trovare le città con un aeroporto di cui non è noto il numero di piste */
+
+SELECT citta
+FROM aeroporto
+WHERE numpiste is null
+
+/* Di ogni volo misto (merci e passeggeri) estrarre il codice e 
+ i dati relativi al trasporto */
+
+ SELECT *
+ FROM volo 
+ WHERE 
