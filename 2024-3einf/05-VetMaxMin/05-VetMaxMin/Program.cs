752d96d6c18d30bcf26422c9baf0fc72a928c6db
Diego Belliardo <diego.belliardo@vallauri.edu>
2024-09-23 09:47:12 +0200

    Es. vettori 3E

---
+++ 2024-3einf/05-VetMaxMin/05-VetMaxMin/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_VetMaxMin
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int[] vet;
+            int n,min,max;
+            Random rnd = new Random();
+
+            Console.Write("Lunghezza vettore: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            vet= new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = rnd.Next(1,100);
+            }
+            //max = int.MinValue;
+            //min= int.MaxValue;
+            max = min = vet[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                if (vet[i] > max)
+                {
+                    max = vet[i];
+                }
+                if (vet[i] < min)
+                {
+                    min = vet[i];
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(vet[i]+" ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Il valore minimo è: " + min);
+            Console.WriteLine("Il valore massimo è: " + max);
+
+            Console.ReadKey();
+        }
+    }
+}
