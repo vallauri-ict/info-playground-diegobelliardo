2e346df6508a4af0755ce075d4f23d34ea2de6c9
Diego Belliardo <diego.belliardo@vallauri.edu>
2024-10-16 08:52:41 +0200

    Iniziato es MetaVettore 3E

---
+++ 2024-3einf/09-MetaVettore_Solution/09-MetaVettore/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Caricare un vettore, chiedendo la lunghezza(numero pari), con numeri casuali 
+ * tra 0 e 100. Quindi copiare la prima metà degli elmenti del vettore in un 
+ * vettore B in ordine inverso e copiare la seconda metà del vettore in un vettore
+ *  C iserendo in modo alternato il primo e l'ultimo elemento, il secondo e il
+ *  penultimo e così via
+ */
+
+
+namespace _09_MetaVettore
+{
+    internal class Program
+    {
+        Random rnd = new Random();  //Variabile globale
+        static void Main(string[] args)
+        {
+            int n; // Variabile locale
+
+            Console.Write("Inserisci la lunghezza del vettore (numero pari): ");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2024-4binf/01ObjectTest/01ObjectTest/Quadrato.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01ObjectTest
+{
+    internal class Quadrato
+    {
+        private int lato;
+        private Quadrato() { }
+        private Quadrato(int lato) { 
+            this.lato = lato;
+        }
+
+        public int Lato { get => lato; }
+
+        public static Quadrato Create(int lato) { 
+            if (lato >= 0) return new Quadrato(lato); 
+            else return null;
+        }
+    }
+}
