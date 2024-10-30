19121ef6a601de317a56f0980835505557eb14c8
Diego Belliardo <diego.belliardo@vallauri.edu>
2024-10-30 08:51:38 +0100

    [3E] Es matrice spirale 41 p 85
    Non funziona provare a usare un solo ciclo e 2 variabili per
    decidere di quanto devono essere modificati i e j

---
+++ 2024-3einf/11-Es41p84_solution/11-Es41p84/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Es41p84
+{
+    internal class Program
+    {
+        static private int[,] mat;
+        static void Main(string[] args)
+        {
+            int r,cont=1,i=0,j=0;
+
+            Console.Write("Inserisci la dimensione della matrice quadrata: ");
+            r=int.Parse(Console.ReadLine());
+            mat = new int[r, r];
+
+            
+            while (i < r)
+            {
+                while (j < r)
+                {
+                    mat[i,j] = cont;
+                    cont++;
+
+                    j++;
+                }
+                
+                i++;
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
