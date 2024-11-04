01dd6db202c250b4b90959b2edd058bed5deceaf
Diego Belliardo <diego.belliardo@vallauri.edu>
2024-11-04 09:44:26 +0100

    [3E] Es 44 pag 85 fatto

---
+++ 2024-3einf/12-Es 42 pag 85/Es 41 pag 85/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_41_pag_85
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = InserisciValore("Inserire le dimensioni della matrice (n>0): ", 2), i = 0,cont = 1, j = 0, valore = 0;
+            int[,] mat = new int[r, r];
+            while (cont <= r / 2)
+            {
+                j = cont-1;
+                do
+                {
+                    valore++;
+                    mat[i, j] = valore;
+                    j++;
+                } while (j < r - cont);
+                while (i < r - cont)
+                {
+                    valore++;
+                    mat[i, j] = valore;
+                    i++;
+                }
+                while (j >= cont)
+                {
+                    valore++;
+                    mat[i, j] = valore;
+                    j--;
+                }
+                while (i >= cont)
+                {
+                    valore++;
+                    mat[i, j] = valore;
+                    i--;
+                }
+                cont++;
+                i++;
+            }
+            if(r%2!=0)
+                mat[r/2,r/2] = ++valore;
+            StampaMatrice(mat);
+            Console.ReadKey();
+        }
+
+        private static void StampaMatrice(int[,] mat)
+        {
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    Console.Write(mat[i, j].ToString().PadLeft(4));
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static int InserisciValore(string mess = "Inserire un valore:", int min = 0, int max = int.MaxValue)
+        {
+            int n;
+            do
+            {
+                Console.WriteLine(mess);
+            } while (!int.TryParse(Console.ReadLine(), out n) || n < min || n > max);
+            return n;
+        }
+    }
+}
---
+++ 2024-3einf/13-Es44p85_solution/13-Es44p85/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Es44p85
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int r, c;
+            int[,] mat;
+
+            Console.Write("Inserisci il numero di righe: ");
+            r=int.Parse(Console.ReadLine());
+            Console.Write("Inserisci il numero di colonne: ");
+            c = int.Parse(Console.ReadLine());
+            mat=new int[r,c];
+
+            inserisciOrdinato(mat);
+            stampaMat(mat);
+            Console.WriteLine();
+            if (matOrdinata(mat))
+            {
+                Console.WriteLine("La matrice è ordinata!!");
+            }
+            else
+            {
+                Console.WriteLine("La matrice NON è ordinata!!");
+            }
+            Console.ReadKey();
+        }
+
+        private static void stampaMat(int[,] mat)
+        {
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    Console.Write(mat[i,j].ToString().PadLeft(3));
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static bool matOrdinata(int[,] mat)
+        {
+            bool ordinata = true;
+            int aus=int.MinValue;
+
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    if(aus== int.MinValue)
+                    {
+                        aus = mat[i,j];
+                    }
+                    else
+                    {
+                        if (!(mat[i,j] >= aus))
+                        {
+                            ordinata = false;
+                        }
+                    }
+                }
+            }
+
+            return ordinata;
+        }
+
+        private static void inserisciOrdinato(int[,] mat)
+        {
+            int val = 1;
+
+            for(int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1)-1; j++)
+                {
+                    mat[i, j] = val++;
+                }
+            }
+        }
+    }
+}
