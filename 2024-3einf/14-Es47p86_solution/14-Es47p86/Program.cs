9f9f4dbe915d50e860d6c6df9c7d8d0b4db09524
Diego Belliardo <diego.belliardo@vallauri.edu>
2024-11-07 12:31:51 +0100

    [3E] Es 47 p 86

---
+++ 2024-3einf/14-Es47p86_solution/14-Es47p86/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Es47p86
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] mat1,mat2;
+            int r;
+
+            Console.Write("Dimensione delle matrici: ");
+            r=int.Parse(Console.ReadLine());
+
+            mat1 = new int[r, r];
+            mat2 = new int[r, r];
+
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < r; j++)
+                {
+                    Console.Clear();
+                    stampaMat(mat1);
+                    Console.WriteLine();
+                    stampaMat(mat2);
+                    Console.WriteLine();
+                    Console.Write($"Insersici l'elemento [{i},{j}]: ");
+                    mat1[i, j] = int.Parse(Console.ReadLine());
+                    mat2[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+            if (diagonaliUguali(mat1,mat2))
+            {
+                Console.WriteLine("Le diagonali delle matrici sono uguali");
+            }
+            else
+            {
+                Console.WriteLine("Le diagonali delle matrici NON sono uguali");
+            }
+            Console.ReadKey();
+        }
+
+        private static bool diagonaliUguali(int[,] mat1, int[,] mat2)
+        {
+            bool ugualiDiag = true;
+            int i = 0, j ;
+
+            while (i < mat1.GetLength(0) && ugualiDiag)
+            {
+                j = 0;
+                while (j < mat1.GetLength(0) && ugualiDiag)
+                {
+                    if (i==j && mat1[i, j] != mat2[i,j])
+                    {
+                        ugualiDiag = false;
+                    }
+                    if (j==(mat1.GetLength(0) - i -1) && mat1[i, j] != mat2[i, j])
+                    {
+                        ugualiDiag = false;
+                    }
+                    j++;
+                }
+                i++;
+            }
+
+            return ugualiDiag;
+        }
+
+        private static void stampaMat(int[,] mat1)
+        {
+            for (int i = 0;i < mat1.GetLength(0); i++)
+            {
+                for(int j = 0;j < mat1.GetLength(1); j++)
+                {
+                    Console.Write(mat1[i, j].ToString().PadLeft(3));
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 2024-3einf/14-Es47p86_solution/14-Es47p86/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente 
+// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+// associate a un assembly.
+[assembly: AssemblyTitle("14-Es47p86")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("14-Es47p86")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili
+// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da
+// COM, impostare su true l'attributo ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+[assembly: Guid("ea85999f-a84c-4a69-9d94-53f031e80bd2")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+//
+//      Versione principale
+//      Versione secondaria
+//      Numero di build
+//      Revisione
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
