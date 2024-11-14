4f24a62e21005dc26b86c0a41783d8fc9b4e75c0
Diego Belliardo <diego.belliardo@vallauri.edu>
2024-11-14 12:29:58 +0100

    [3E] 58 p 86 triangolo di tartaglia terminato

--- 2024-3einf/22-Es58p86_solution/22-Es58p86/Program.cs
+++ 2024-3einf/22-Es58p86_solution/22-Es58p86/Program.cs
@@ -11,16 +11,14 @@ namespace _22_Es58p86
         static void Main(string[] args)
         {
             int[,] mat;
-            int r, centro,cont;
+            int r, centro, offset;
 
             Console.Write("Inserisci dimensione matrice rxr: ");
             r = int.Parse(Console.ReadLine());
 
             mat = new int[r, r];
-            centro = (int)Math.Ceiling((decimal)(r / 2.0));
-
-            //Math.Round()
-
+            //centro = (int)Math.Ceiling((decimal)(r / 2.0)); Non necessario
+            centro = r / 2;
             /*
              * 0 0 0 1 0 0 0
              * 0 0 1   1 0 0
@@ -29,8 +27,41 @@ namespace _22_Es58p86
              *   4   6   4
              */
 
+            offset = 0;
+            for (int i = 0; i < mat.GetLength(0)/2+1; i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    if (centro-offset==j || centro+offset==j)
+                    {
+                        mat[i, j] = 1;
+                    }
+                    if (i > 1 && j>1 && j< mat.GetLength(1)-1 && 
+                        mat[i - 1, j - 1] != 0 && mat[i - 1, j + 1] != 0)
+                    {
+                        mat[i, j] = mat[i - 1, j - 1] + mat[i -1, j + 1];
+                    }
+                }
+                offset++;
+            }
+
+            stampaMat(mat);
+
             Console.ReadKey();
 
+        }
+
+        private static void stampaMat(int[,] mat1)
+        {
+            for (int i = 0; i < mat1.GetLength(0)/2+1; i++)
+            {
+                for (int j = 0; j < mat1.GetLength(1); j++)
+                {
+                    if (mat1[i, j] == 0) Console.Write(" ".PadLeft(5));
+                    else Console.Write(mat1[i, j].ToString().PadLeft(5));
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
