a35a8f701d50b45ab035a7dea9000dd8153229b9
Diego Belliardo <diego.belliardo@vallauri.edu>
2024-11-30 08:48:39 +0100

    [3E] Es 67 pag 68

---
+++ 2024-3einf/25-Es67p86_solution/25-Es67p86/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_Es67p86
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1, s2;
+            
+            Console.Write("Inserisci la prima stringa: ");
+            s1=Console.ReadLine();
+            Console.Write("Inserisci la stringa da ricercare: ");
+            s2=Console.ReadLine();
+
+            Console.WriteLine($"La stringa è stata trovata {Ricerca(s1, s2)} volte");
+
+            Console.ReadKey();
+        }
+
+        private static int Ricerca(string s1, string s2)
+        {
+            int cont = 0,pos;
+
+            do
+            {
+                pos = s1.IndexOf(s2);
+                if (pos != -1)
+                {
+                    cont++;
+                    s1=s1.Substring(pos+s2.Length);
+                }
+            } while (pos != -1);
+
+            return cont;
+        }
+    }
+}
