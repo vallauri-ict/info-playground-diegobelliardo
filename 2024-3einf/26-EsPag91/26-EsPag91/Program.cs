41177be2c286d31eca1602afbb2e7814089771b0
Diego Belliardo <diego.belliardo@vallauri.edu>
2024-12-02 09:40:26 +0100

    [3E] Es ricerca sequenziale

---
+++ 2024-3einf/26-EsPag91/26-EsPag91/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26_EsPag91
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] v = {"Bianchi", "Devirgilio", "Fabbri", "Gallo",
+                "Melandri", "Varese" };
+            string nome = "";
+            int i = 0;
+
+            Console.Write("Insersci il nome da cercare: ");
+            nome=Console.ReadLine();
+
+            //while (i < v.Length && v[i] != nome)
+            //{
+            //    i++;
+            //}
+            //if (i<v.Length) Console.WriteLine($"Elemento trovato in posizione {i}");
+            //else Console.WriteLine("Elemento NON trovato");
+
+            // Soluzione 2
+            while (i < v.Length-1 && v[i] != nome)
+            {
+                i++;
+            }
+            
+            if (v[i]==nome) Console.WriteLine($"Elemento trovato in posizione {i}");
+            else Console.WriteLine("Elemento NON trovato");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2024-3einf/27-EsPag92/27-EsPag92/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_EsPag92
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] v = {"Bianchi", "Devirgilio", "Gallo",
+                "Melandri", "Fabbri", "Varese", "Bianchi" };
+            string nome = "";
+            bool trovato = false;
+
+            Console.Write("Insersci il nome da cercare: ");
+            nome = Console.ReadLine();
+
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (v[i] == nome)
+                {
+                    Console.WriteLine("Elemento trovato in posizione " + i);
+                    trovato = true;
+                }
+            }
+
+            if (!trovato)
+                Console.WriteLine("Elemento non trovato");
+
+            Console.ReadKey();
+        }
+    }
+}
