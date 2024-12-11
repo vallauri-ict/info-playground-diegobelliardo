c81de4e8256273716a0adc15e703a733522893ee
Diego Belliardo <diego.belliardo@vallauri.edu>
2024-12-11 08:50:03 +0100

    [3E] Iniziato Selection Sort p.103

---
+++ 2024-3einf/29-EsP103-SelectionSort/29-EsP103-SelectionSort/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29_EsP103_SelectionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = { 12, 2, 13, 15, 18 };
+
+
+        }
+    }
+}
