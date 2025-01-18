757ad2492c50c91d709aba4ce3eda61176873888
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-01-18 08:51:40 +0100

    [3E] Ricorsione Fattoriale + inizio SelectionSort

---
+++ 2024-3einf/32-RicorsioneFattoriale/32-RicorsioneFattoriale/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32_RicorsioneFattoriale
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,ris;
+
+            Console.WriteLine("CALCOLO DEL FATOTRIALE\n\n");
+            Console.Write("Inserisci il numero: ");
+            n=Convert.ToInt32(Console.ReadLine());
+
+            ris = Fattoriale(n);
+            Console.WriteLine("\n\nRISULTATO: " + ris);
+
+            Console.ReadKey();
+        }
+
+        private static int Fattoriale(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Fattoriale(n-1);
+            }
+        }
+    }
+}
---
+++ 2024-3einf/33-RicorsioneSelectionS/33-RicorsioneSelectionS/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _33_RicorsioneSelectionS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[10];
+            int[] vr = new int[10];
+            Random r = new Random();
+
+            Console.WriteLine("SELECTION SORT RICORSIVO\n");
+            CaricaVet(v, r);
+            CopiaVet(v, vr);
+            StampaVet(v);
+            SelectionSort(v);
+            SelectionSortRicorsivo(v);
+
+        }
+
+        private static void SelectionSortRicorsivo(int[] v)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void CopiaVet(int[] v, int[] vr)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void SelectionSort(int[] v)
+        {
+            
+        }
+
+        private static void StampaVet(int[] v)
+        {
+            for (int i = 0; i< v.Length; i++)
+            {
+                Console.Write(v[i].ToString().PadLeft(3));
+            }
+        }
+
+        private static void CaricaVet(int[] v, Random r)
+        {
+            for (int i = 0; i < v.Length; i++)
+            {
+                v[i] = r.Next(1, 100);
+            }
+        }
+    }
+}
