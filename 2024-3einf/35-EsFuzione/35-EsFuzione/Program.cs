4b2cdd728a140607acc4961683d5a5aabfa99fdc
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-01-22 08:51:19 +0100

    [3E] Fusione vettori - Iniziato

---
+++ 2024-3einf/35-EsFuzione/35-EsFuzione/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _35_EsFuzione
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int scelta;
+            do
+            {
+                scelta = visualizzaMenu();
+                switch (scelta)
+                {
+                    case 0:
+                        Console.WriteLine("Ciao ciao...");
+                        break;
+                    case 1:
+                        Console.WriteLine("Caso 1");
+                        break;
+                    case 2:
+                        Console.WriteLine("Caso 2");
+                        break;
+                    default:
+                        Console.WriteLine("Scelta non disponibile!");
+                        break;
+                }
+                Console.ReadKey();
+            }
+            while (scelta != 0);
+        }
+
+        private static int visualizzaMenu()
+        {
+            int scelta;
+            Console.Clear();
+            Console.WriteLine("******  FUSIONE DI VETTORI  ******");
+            Console.WriteLine("1) Metodo Base");
+            Console.WriteLine("2) Merge con Tappo");
+            Console.WriteLine("0) ESCI");
+            Console.WriteLine("\nScelta > ");
+            while (!int.TryParse(Console.ReadLine(), out scelta));
+
+            return scelta;
+        }
+    }
+}
---
+++ 2024-3einf/35-EsFuzione/35-EsFuzione/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente 
+// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+// associate a un assembly.
+[assembly: AssemblyTitle("35-EsFuzione")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("35-EsFuzione")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili
+// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da
+// COM, impostare su true l'attributo ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+[assembly: Guid("84479e91-ddf4-47f2-b4b2-56795682d0b5")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+//
+//      Versione principale
+//      Versione secondaria
+//      Numero di build
+//      Revisione
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
