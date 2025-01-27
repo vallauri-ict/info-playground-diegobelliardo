9eea33e13d60185fae453712df26251649b4f167
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-01-27 08:52:47 +0100

    [5B] Primo es Trigger

---
+++ 2024-5binf/06-Trigger01/TriggerLogProdotti.sql
@@ -0,0 +1,15 @@
+﻿CREATE TRIGGER [LogModifiche]
+	ON Prodotti
+	AFTER UPDATE
+	AS
+	BEGIN
+		SET NOCOUNT ON
+
+		INSERT INTO AuditLog (idProdotto,action,descrizione)
+		SELECT inserted.Id,
+		CONCAT('Update Prodotti: ',inserted.nome,
+		'| vecchio Prezzo: ',deleted.prezzo,
+		'| nuovo Prezzo: ',inserted.prezzo,
+
+
+	END
