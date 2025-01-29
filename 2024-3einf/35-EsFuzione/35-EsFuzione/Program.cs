e3f8f9be888267381174d78d5aa06aed98d33d34
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-01-29 08:51:20 +0100

    [5B-3E] Trigger es vari - Fusione con tappo (da sistemare)

--- 2024-3einf/35-EsFuzione/35-EsFuzione/Program.cs
+++ 2024-3einf/35-EsFuzione/35-EsFuzione/Program.cs
@@ -46,6 +46,7 @@ namespace _35_EsFuzione
                         break;
                     case 2:
                         Console.WriteLine("Caso 2");
+                        FusioneTappo();
                         break;
                     default:
                         Console.WriteLine("Scelta non disponibile!");
@@ -54,6 +55,39 @@ namespace _35_EsFuzione
                 Console.ReadKey();
             }
             while (scelta != 0);
+        }
+
+        private static void FusioneTappo()
+        {
+            StampaVet(a, "Vettore a:");
+            StampaVet(b, "Vettore b:");
+
+            const string TAPPO = "zzz";
+
+            Array.Resize(ref a, a.Length + 1);
+            Array.Resize(ref b, b.Length + 1);
+
+            a[a.Length-1] = b[b.Length-1] = TAPPO;
+
+            string[] c = new string[a.Length + b.Length];
+
+            int i = 0; int j = 0; 
+
+            for (int k = 0; k < c.Length; k++)
+            {
+                if (a[i].CompareTo(b[j]) > 0)
+                {
+                    c[k] = b[j];
+                    j++;
+                }
+                else
+                {
+                    c[k] = a[i];
+                    i++;
+                }
+            }
+
+            StampaVet(c, "Vettore C: ");
         }
 
         private static void FusioneBase()
@@ -113,6 +147,7 @@ namespace _35_EsFuzione
         private static void StampaVet(string[] a, string message)
         {
             Console.WriteLine(message);
+            a=new string[3];
             for (int i = 0; i < a.Length; i++) { 
                 Console.Write(a[i].PadLeft(12));
                 if ((i + 1) % 6 == 0) { 
