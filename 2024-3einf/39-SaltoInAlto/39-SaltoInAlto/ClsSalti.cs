8911e458598d4a9935f315313b968ed783293b8c
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-01-30 12:31:14 +0100

    [3E] Iniziato es salto in alto

---
+++ 2024-3einf/39-SaltoInAlto/39-SaltoInAlto/ClsSalti.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _39_SaltoInAlto
+{
+    internal class ClsSalti
+    {
+        static Random rnd = new Random();
+        internal static void CaricaNazioni(string[] nazione)
+        {
+            string[] stati = { "Italia", "Cossovo", "Jamaica", "Francia" };
+            for (int i = 0; i < nazione.Length; i++)
+            {
+                int index = rnd.Next(stati.Length);
+                nazione[i] = stati[index];
+            }
+        }
+
+        internal static void CaricaSalti(int[] salti)
+        {
+            for (int i = 0;i < salti.Length; i++)
+            {
+                int misura = rnd.Next(190,240);
+                salti[i] = misura;
+            }
+        }
+
+        internal static void CaricaSesso(string[] sesso)
+        {
+            for(int i = 0; i<sesso.Length; i++)
+            {
+                int sex = rnd.Next(2);
+                sesso[i] = sex == 0 ? "M" : "F";
+            }
+        }
+    }
+}
---
+++ 2024-3einf/39-SaltoInAlto/39-SaltoInAlto/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _39_SaltoInAlto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] atleti = {"Ambrogio","Bodoaga","Borbone","Burdisso",
+                "Cerone","Dzeljo","Ferrero","Fontana","Hoxha","Kardash" };
+            string[] nazione = new string[atleti.Length];
+            string[] sesso= new string[atleti.Length];
+            int[] salti=new int[atleti.Length];
+
+            ClsSalti.CaricaNazioni(nazione);
+            ClsSalti.CaricaSesso(sesso);
+            ClsSalti.CaricaSalti(salti);
+
+            int scelta;
+            do
+            {
+                scelta = visualizzaMenu();
+                switch (scelta)
+                {
+                    case 1:
+                        Console.WriteLine(scelta);
+                        break;
+                    case 2:
+                        Console.WriteLine(scelta);
+                        break;
+                    case 3:
+                        Console.WriteLine(scelta);
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 0:
+                        Console.WriteLine(scelta);
+                        break;
+                }
+                Console.ReadKey();
+
+            }
+            while (scelta != 0);
+        }
+
+        private static int visualizzaMenu()
+        {
+            int sc;
+
+            Console.Clear();
+            Console.WriteLine("---   SALTO IN ALTO   ---");
+            Console.WriteLine("1: Visualizza Dati");
+            Console.WriteLine("2: Classifica Generale");
+            Console.WriteLine("3: Media Salti Nazione in Input");
+            Console.WriteLine("4: Media Salti di ogni Nazione");
+            Console.WriteLine("5: Classifica per Nazioni");
+            Console.WriteLine("6: Media salti uomini e donne");
+            Console.WriteLine("7: Cercare i vincitori M e F della nazione prima classificata");
+            Console.WriteLine("0: Esci");
+            while (!int.TryParse(Console.ReadLine(), out sc) && sc<=7 && sc>=0);
+
+            return sc;
+        }
+
+        private static void CaricaSalti(int[] salti)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
