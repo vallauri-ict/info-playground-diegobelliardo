41e37baa8c58747cd1a2d5091fc2ddfd59a02f30
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-02-20 12:30:30 +0100

    [3E] Iniziato F4 dato da finire a casa

---
+++ 2024-3einf/42-ForzaQuattro/42-ForzaQuattro/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace _42_ForzaQuattro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvGioco = new System.Windows.Forms.DataGridView();
+            this.btnInizia = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGioco)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvGioco
+            // 
+            this.dgvGioco.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvGioco.Location = new System.Drawing.Point(13, 13);
+            this.dgvGioco.Name = "dgvGioco";
+            this.dgvGioco.Size = new System.Drawing.Size(240, 150);
+            this.dgvGioco.TabIndex = 0;
+            this.dgvGioco.SelectionChanged += new System.EventHandler(this.dgvGioco_SelectionChanged);
+            // 
+            // btnInizia
+            // 
+            this.btnInizia.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInizia.Location = new System.Drawing.Point(13, 182);
+            this.btnInizia.Name = "btnInizia";
+            this.btnInizia.Size = new System.Drawing.Size(240, 40);
+            this.btnInizia.TabIndex = 1;
+            this.btnInizia.Text = "INIZIA";
+            this.btnInizia.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 237);
+            this.Controls.Add(this.btnInizia);
+            this.Controls.Add(this.dgvGioco);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvGioco)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvGioco;
+        private System.Windows.Forms.Button btnInizia;
+    }
+}
+
---
+++ 2024-3einf/42-ForzaQuattro/42-ForzaQuattro/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _42_ForzaQuattro
+{
+    public partial class Form1 : Form
+    {
+        private const int NUM_COL=7;
+        private const int NUM_ROW = 6;
+        char[,] f4 = new char[NUM_ROW, NUM_COL];
+
+        int turno;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Setdgv();
+        }
+
+        private void Setdgv()
+        {
+            dgvGioco.RowCount = NUM_ROW; dgvGioco.ColumnCount = NUM_COL;
+
+            for (int i = 0; i < NUM_ROW; i++)
+            {
+                dgvGioco.Rows[i].Height = dgvGioco.Size.Height / NUM_ROW;
+                dgvGioco.Columns[i].Width = dgvGioco.Size.Width / NUM_COL;
+            }
+
+            // nascondo intestazioni di riga e colonna
+            dgvGioco.RowHeadersVisible = false;
+            dgvGioco.ColumnHeadersVisible = false;
+            // nascondo le scrollBars
+            dgvGioco.ScrollBars = ScrollBars.None;
+            dgvGioco.AllowUserToResizeColumns = false;
+            dgvGioco.AllowUserToResizeRows = false;
+            dgvGioco.ReadOnly = true;
+
+            svuotaF4();
+            visualizzaF4();
+        }
+
+        private void visualizzaF4()
+        {
+            for (int i = 0; i < NUM_ROW; i++)
+            {
+                for (int j = 0; j < NUM_COL; j++)
+                {
+                    dgvGioco.Rows[i].Cells[j].Style.BackColor = 
+                        f4[i, j] == 'R'? Color.Red :
+                        f4[i,j] == 'G'? Color.Yellow:Color.Transparent;
+                }
+            }
+        }
+
+        private void svuotaF4()
+        {
+            for (int i = 0; i < NUM_ROW; i++)
+            {
+                for (int j = 0; j < NUM_COL; j++)
+                {
+                    f4[i, j] = ' ';
+                }
+            }
+        }
+
+        private void dgvGioco_SelectionChanged(object sender, EventArgs e)
+        {
+            dgvGioco.ClearSelection();
+        }
+    }
+}
