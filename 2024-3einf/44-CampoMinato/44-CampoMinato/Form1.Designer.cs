9f2a4f39507c4c079e6af4f5c82779446069625d
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-03-10 09:45:36 +0100

    [3E] Iniziato Campo minato

---
+++ 2024-3einf/44-CampoMinato/44-CampoMinato/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace _44_CampoMinato
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCampoMinato = new System.Windows.Forms.DataGridView();
+            this.btnInizia = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCampoMinato)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCampoMinato
+            // 
+            this.dgvCampoMinato.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCampoMinato.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dgvCampoMinato.Location = new System.Drawing.Point(0, 0);
+            this.dgvCampoMinato.Name = "dgvCampoMinato";
+            this.dgvCampoMinato.Size = new System.Drawing.Size(414, 365);
+            this.dgvCampoMinato.TabIndex = 0;
+            // 
+            // btnInizia
+            // 
+            this.btnInizia.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInizia.Location = new System.Drawing.Point(0, 360);
+            this.btnInizia.Name = "btnInizia";
+            this.btnInizia.Size = new System.Drawing.Size(414, 84);
+            this.btnInizia.TabIndex = 1;
+            this.btnInizia.Text = "INIZIA";
+            this.btnInizia.UseVisualStyleBackColor = true;
+            this.btnInizia.Click += new System.EventHandler(this.btnInizia_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 441);
+            this.Controls.Add(this.btnInizia);
+            this.Controls.Add(this.dgvCampoMinato);
+            this.Name = "Form1";
+            this.Text = "Campo Minato";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCampoMinato)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCampoMinato;
+        private System.Windows.Forms.Button btnInizia;
+    }
+}
+
---
+++ 2024-3einf/44-CampoMinato/44-CampoMinato/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _44_CampoMinato
+{
+    public partial class Form1 : Form
+    {
+        const int DIM_CAMPO = 9;
+        const int NUM_BOMBE = 10;
+        Random rnd = new Random();
+        int[,] campo = new int[DIM_CAMPO+2, DIM_CAMPO+2];
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InizializzaCampo();
+            SettaDgv();
+        }
+
+        private void SettaDgv()
+        {
+            // imposto numero righe e colonne
+            dgvCampoMinato.ColumnCount = DIM_CAMPO;
+            dgvCampoMinato.RowCount = DIM_CAMPO;
+
+
+        }
+
+        private void InizializzaCampo()
+        {
+            // Posizionamento delle bombe
+            // 0 = no bombe intorno
+            // -1 = bomba
+            // 1-8 = numero di bombe intorno
+
+            int x, y;
+
+            for (int i = 0; i < NUM_BOMBE; i++)
+            {
+                do
+                {
+                    x = rnd.Next(1, DIM_CAMPO+1);
+                    y = rnd.Next(1, DIM_CAMPO+1);
+                } while (campo[x, y] == -1);
+                campo[x, y] = -1;
+            }
+            for (int i = 1; i <= DIM_CAMPO; i++)
+            {
+                for (int j = 1; j <= DIM_CAMPO; j++)
+                {
+                    if (campo[i, j] != -1)
+                    {
+                        campo[i, j] = ContaBombeVicino(i, j);
+                    }
+                }
+            }
+
+        }
+
+        private int ContaBombeVicino(int i, int j)
+        {
+            int contatore = 0;
+            for (int x = i - 1; x <= i + 1; x++)
+            {
+                for (int y = j - 1; y <= j + 1; y++)
+                {
+                    if (campo[x, y] == -1)
+                    {
+                        contatore++;
+                    }
+                }
+            }
+            return contatore;
+        }
+
+        private void btnInizia_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
