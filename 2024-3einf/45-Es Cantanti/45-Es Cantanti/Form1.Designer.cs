f1dbe56ff3450d938202cd9c3feb5a138535138f
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-03-19 08:50:19 +0100

    [3E] Es Canzoni

--- 2024-3einf/45-Es Cantanti/45-Es Cantanti/Form1.Designer.cs
+++ 2024-3einf/45-Es Cantanti/45-Es Cantanti/Form1.Designer.cs
@@ -28,13 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvCantanti = new System.Windows.Forms.DataGridView();
+            this.dgvCanzoni = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCanzoniVendute = new System.Windows.Forms.Button();
+            this.btnClassificaCantanti = new System.Windows.Forms.Button();
+            this.dgvCanzoniRis = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCantanti)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCanzoni)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCanzoniRis)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CANTANTI";
+            // 
+            // dgvCantanti
+            // 
+            this.dgvCantanti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCantanti.Location = new System.Drawing.Point(13, 30);
+            this.dgvCantanti.Name = "dgvCantanti";
+            this.dgvCantanti.Size = new System.Drawing.Size(338, 245);
+            this.dgvCantanti.TabIndex = 1;
+            // 
+            // dgvCanzoni
+            // 
+            this.dgvCanzoni.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCanzoni.Location = new System.Drawing.Point(386, 30);
+            this.dgvCanzoni.Name = "dgvCanzoni";
+            this.dgvCanzoni.Size = new System.Drawing.Size(338, 245);
+            this.dgvCanzoni.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(386, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "CANZONI";
+            // 
+            // btnCanzoniVendute
+            // 
+            this.btnCanzoniVendute.Location = new System.Drawing.Point(13, 303);
+            this.btnCanzoniVendute.Name = "btnCanzoniVendute";
+            this.btnCanzoniVendute.Size = new System.Drawing.Size(338, 59);
+            this.btnCanzoniVendute.TabIndex = 4;
+            this.btnCanzoniVendute.Text = "Ricevuto in ingresso il nome del cantante, contare il numero totale di conzoni ve" +
+    "ndute, visualizzandole in un DataGridView";
+            this.btnCanzoniVendute.UseVisualStyleBackColor = true;
+            // 
+            // btnClassificaCantanti
+            // 
+            this.btnClassificaCantanti.Location = new System.Drawing.Point(386, 303);
+            this.btnClassificaCantanti.Name = "btnClassificaCantanti";
+            this.btnClassificaCantanti.Size = new System.Drawing.Size(338, 59);
+            this.btnClassificaCantanti.TabIndex = 5;
+            this.btnClassificaCantanti.Text = "Dopo aver calcolato per ogni cantante il totale venduto delle loro canzoni, visua" +
+    "lizzarne la classifica";
+            this.btnClassificaCantanti.UseVisualStyleBackColor = true;
+//            this.btnClassificaCantanti.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgvCanzoniRis
+            // 
+            this.dgvCanzoniRis.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCanzoniRis.Location = new System.Drawing.Point(12, 368);
+            this.dgvCanzoniRis.Name = "dgvCanzoniRis";
+            this.dgvCanzoniRis.Size = new System.Drawing.Size(338, 245);
+            this.dgvCanzoniRis.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 670);
+            this.Controls.Add(this.dgvCanzoniRis);
+            this.Controls.Add(this.btnClassificaCantanti);
+            this.Controls.Add(this.btnCanzoniVendute);
+            this.Controls.Add(this.dgvCanzoni);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvCantanti);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCantanti)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCanzoni)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCanzoniRis)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvCantanti;
+        private System.Windows.Forms.DataGridView dgvCanzoni;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCanzoniVendute;
+        private System.Windows.Forms.Button btnClassificaCantanti;
+        private System.Windows.Forms.DataGridView dgvCanzoniRis;
     }
 }
 
--- 2024-3einf/45-Es Cantanti/45-Es Cantanti/Form1.cs
+++ 2024-3einf/45-Es Cantanti/45-Es Cantanti/Form1.cs
@@ -12,9 +12,125 @@ namespace _45_Es_Cantanti
 {
     public partial class Form1 : Form
     {
+        string[] cant = {
+            "001|Freddie Mercury|Gruppo (Queen)|UK",
+            "002|Elvis Presley|Solista|US",
+            "003|Michael Jackson|Solista|US",
+            "004|Madonna|Solista|US",
+            "005|David Bowie|Solista|UK",
+            "006|Bono|Gruppo (U2)|IE",
+            "007|Kurt Cobain|Gruppo (Nirvana)|US"
+        };
+
+        string[] canz = {
+            "Bohemian Rhapsody|Rock|001|6000000",
+            "We Will Rock You|Rock|001|4500000",
+            "Can't Help Falling in Love|Pop|002|5000000",
+            "Jailhouse Rock|Rock|002|4000000",
+            "Billie Jean|Pop|003|10000000",
+            "Thriller|Pop|003|9000000",
+            "Like a Prayer|Pop|004|5000000",
+            "Hung Up|Dance|004|4000000",
+            "Heroes|Rock|005|3000000",
+            "Space Oddity|Rock|005|3500000",
+            "With or Without You|Rock|006|5000000",
+            "One|Rock|006|3500000",
+            "Smells Like Teen Spirit|Grunge|007|7000000",
+            "Come as You Are|Grunge|007|4500000",
+            "Lithium|Grunge|007|3000000"
+        };
+
+        public struct canzone
+        {
+            public string titolo;
+            public string genere;
+            public string codCantante;
+            public int copieVendute;
+        }
+        canzone[] canzoni = new canzone[30];
+
+        public struct cantante
+        {
+            public string codice;
+            public string nome;
+            public string tipo;
+            public string nazionalita;
+        }
+        cantante[] cantanti = new cantante[10];
+        int nCantanti, nCanzoni;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            nCantanti = cant.Length;
+            nCanzoni = canz.Length;
+            SettaDgv(dgvCanzoni);
+            SettaDgv(dgvCantanti);
+            SettaDgv(dgvCanzoniRis);
+            CaricaStrutture();
+        }
+
+        private void CaricaStrutture()
+        {
+            for (int i = 0; i < nCantanti; i++)
+            {
+                string[] campi = cant[i].Split('|');
+                cantanti[i].codice = campi[0];
+                cantanti[i].nome = campi[1];
+                cantanti[i].tipo = campi[2];
+                cantanti[i].nazionalita = campi[3];
+                dgvCantanti.Rows.Add(cantanti[i].codice, cantanti[i].nome, cantanti[i].tipo, cantanti[i].nazionalita);
+                /**** OPPURE ****
+                 * 
+                 * dgvCantanti.Rows.Add();
+                 * dgv.Rows[i].Cells[0].Value = cantanti[i].codice;
+                 * dgv.Rows[i].Cells[1].Value = cantanti[i].nome;
+                 * 
+                 * *****************/
+            }
+            for (int i = 0; i < nCanzoni; i++)
+            {
+                string[] campi = canz[i].Split('|');
+                canzoni[i].titolo = campi[0];
+                canzoni[i].genere = campi[1];
+                canzoni[i].codCantante = campi[2];
+                canzoni[i].copieVendute = Convert.ToInt32(campi[3]);
+                dgvCanzoni.Rows.Add(canzoni[i].titolo, canzoni[i].genere, canzoni[i].codCantante, canzoni[i].copieVendute);
+            }
+
+        }
+
+        private void SettaDgv(DataGridView dgv)
+        {
+            dgv.ColumnCount = 4;
+            dgv.ColumnHeadersVisible = true;
+            IntestaTabella(dgv);
+            dgv.RowHeadersVisible = true;
+            dgv.ReadOnly = true;
+            dgv.ClearSelection();
+        }
+
+        private void IntestaTabella(DataGridView dgv)
+        {
+            if(dgv.Name == "dgvCantanti")
+            {
+                dgv.Columns[0].Name = "Codice";
+                dgv.Columns[1].Name = "Nome";
+                dgv.Columns[2].Name = "Tipo";
+                dgv.Columns[3].Name = "Nazionalità";
+            }
+            else
+            {
+                dgv.Columns[0].Name = "Titolo";
+                dgv.Columns[1].Name = "Genere";
+                dgv.Columns[2].Name = "Codice Cantante";
+                dgv.Columns[3].Name = "Copie Vendute";
+            }
         }
     }
 }
