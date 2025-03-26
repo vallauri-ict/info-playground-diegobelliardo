b4afe30e3079b9521de8354c7888a7d70a39aec2
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-03-26 08:50:00 +0100

    Continuazione es Gestione Studenti

---
+++ 2024-3einf/46-GestioneStudenti_Solution/GestioneStudenti/FormMain.Designer.cs
@@ -0,0 +1,395 @@
+﻿namespace GestioneStudenti
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvStudenti = new System.Windows.Forms.DataGridView();
+            this.dgvValutazioni = new System.Windows.Forms.DataGridView();
+            this.btnStudSenzaVoti = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCognome = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.cmbClasse = new System.Windows.Forms.ComboBox();
+            this.btnInserisciStudente = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbMaterie = new System.Windows.Forms.ComboBox();
+            this.nupVoto = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdbLaboratorio = new System.Windows.Forms.RadioButton();
+            this.rdbOrale = new System.Windows.Forms.RadioButton();
+            this.rdbScritto = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbMatricole = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudenti)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvValutazioni)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupVoto)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvStudenti
+            // 
+            this.dgvStudenti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvStudenti.Location = new System.Drawing.Point(8, 8);
+            this.dgvStudenti.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvStudenti.Name = "dgvStudenti";
+            this.dgvStudenti.RowHeadersWidth = 62;
+            this.dgvStudenti.RowTemplate.Height = 28;
+            this.dgvStudenti.Size = new System.Drawing.Size(485, 156);
+            this.dgvStudenti.TabIndex = 0;
+            // 
+            // dgvValutazioni
+            // 
+            this.dgvValutazioni.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvValutazioni.Location = new System.Drawing.Point(505, 8);
+            this.dgvValutazioni.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvValutazioni.Name = "dgvValutazioni";
+            this.dgvValutazioni.RowHeadersWidth = 62;
+            this.dgvValutazioni.RowTemplate.Height = 28;
+            this.dgvValutazioni.Size = new System.Drawing.Size(443, 156);
+            this.dgvValutazioni.TabIndex = 1;
+            // 
+            // btnStudSenzaVoti
+            // 
+            this.btnStudSenzaVoti.Location = new System.Drawing.Point(955, 8);
+            this.btnStudSenzaVoti.Margin = new System.Windows.Forms.Padding(2);
+            this.btnStudSenzaVoti.Name = "btnStudSenzaVoti";
+            this.btnStudSenzaVoti.Size = new System.Drawing.Size(139, 25);
+            this.btnStudSenzaVoti.TabIndex = 2;
+            this.btnStudSenzaVoti.Text = "STUDENTI SENZA VOTI";
+            this.btnStudSenzaVoti.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 179);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "COGNOME:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 205);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "NOME:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 231);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "CLASSE:";
+            // 
+            // txtCognome
+            // 
+            this.txtCognome.Location = new System.Drawing.Point(74, 177);
+            this.txtCognome.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCognome.Name = "txtCognome";
+            this.txtCognome.Size = new System.Drawing.Size(139, 20);
+            this.txtCognome.TabIndex = 6;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(74, 203);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(139, 20);
+            this.txtNome.TabIndex = 7;
+            // 
+            // cmbClasse
+            // 
+            this.cmbClasse.FormattingEnabled = true;
+            this.cmbClasse.Location = new System.Drawing.Point(74, 229);
+            this.cmbClasse.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbClasse.Name = "cmbClasse";
+            this.cmbClasse.Size = new System.Drawing.Size(139, 21);
+            this.cmbClasse.TabIndex = 8;
+            // 
+            // btnInserisciStudente
+            // 
+            this.btnInserisciStudente.Location = new System.Drawing.Point(9, 261);
+            this.btnInserisciStudente.Margin = new System.Windows.Forms.Padding(2);
+            this.btnInserisciStudente.Name = "btnInserisciStudente";
+            this.btnInserisciStudente.Size = new System.Drawing.Size(204, 25);
+            this.btnInserisciStudente.TabIndex = 9;
+            this.btnInserisciStudente.Text = "INSERISCI STUDENTE";
+            this.btnInserisciStudente.UseVisualStyleBackColor = true;
+            this.btnInserisciStudente.Click += new System.EventHandler(this.btnInserisciStudente_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(502, 206);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "VOTO:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(502, 180);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "MATERIA:";
+            // 
+            // cmbMaterie
+            // 
+            this.cmbMaterie.FormattingEnabled = true;
+            this.cmbMaterie.Location = new System.Drawing.Point(578, 176);
+            this.cmbMaterie.Name = "cmbMaterie";
+            this.cmbMaterie.Size = new System.Drawing.Size(139, 21);
+            this.cmbMaterie.TabIndex = 14;
+            // 
+            // nupVoto
+            // 
+            this.nupVoto.Location = new System.Drawing.Point(578, 203);
+            this.nupVoto.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nupVoto.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nupVoto.Name = "nupVoto";
+            this.nupVoto.Size = new System.Drawing.Size(139, 20);
+            this.nupVoto.TabIndex = 15;
+            this.nupVoto.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdbLaboratorio);
+            this.groupBox1.Controls.Add(this.rdbOrale);
+            this.groupBox1.Controls.Add(this.rdbScritto);
+            this.groupBox1.Location = new System.Drawing.Point(505, 231);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(212, 100);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "TIPO";
+            // 
+            // rdbLaboratorio
+            // 
+            this.rdbLaboratorio.AutoSize = true;
+            this.rdbLaboratorio.Location = new System.Drawing.Point(42, 65);
+            this.rdbLaboratorio.Name = "rdbLaboratorio";
+            this.rdbLaboratorio.Size = new System.Drawing.Size(102, 17);
+            this.rdbLaboratorio.TabIndex = 2;
+            this.rdbLaboratorio.TabStop = true;
+            this.rdbLaboratorio.Text = "LABORATORIO";
+            this.rdbLaboratorio.UseVisualStyleBackColor = true;
+            // 
+            // rdbOrale
+            // 
+            this.rdbOrale.AutoSize = true;
+            this.rdbOrale.Location = new System.Drawing.Point(42, 42);
+            this.rdbOrale.Name = "rdbOrale";
+            this.rdbOrale.Size = new System.Drawing.Size(61, 17);
+            this.rdbOrale.TabIndex = 1;
+            this.rdbOrale.TabStop = true;
+            this.rdbOrale.Text = "ORALE";
+            this.rdbOrale.UseVisualStyleBackColor = true;
+            // 
+            // rdbScritto
+            // 
+            this.rdbScritto.AutoSize = true;
+            this.rdbScritto.Location = new System.Drawing.Point(42, 19);
+            this.rdbScritto.Name = "rdbScritto";
+            this.rdbScritto.Size = new System.Drawing.Size(72, 17);
+            this.rdbScritto.TabIndex = 0;
+            this.rdbScritto.TabStop = true;
+            this.rdbScritto.Text = "SCRITTO";
+            this.rdbScritto.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(502, 338);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "MATRICOLA:";
+            // 
+            // cmbMatricole
+            // 
+            this.cmbMatricole.FormattingEnabled = true;
+            this.cmbMatricole.Location = new System.Drawing.Point(579, 335);
+            this.cmbMatricole.Name = "cmbMatricole";
+            this.cmbMatricole.Size = new System.Drawing.Size(139, 21);
+            this.cmbMatricole.TabIndex = 18;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 296);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 43);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "RICERCA STUDENTE IN BASE ALLA MATRICOLA";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 343);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(204, 43);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "RICERCA STUDENTE IN BASE A COGNOME E NOME";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(9, 390);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(204, 43);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "ORDINA STUDENTE IN BASE AL NOMINATIVO";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(10, 437);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(203, 43);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "CONTA NUMERO STUDENTI IN CLASSE";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(10, 484);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(203, 43);
+            this.button5.TabIndex = 23;
+            this.button5.Text = "CONTA NUMERO DI VOTI DI STUDENTI CLASSE VIA COMBO";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 529);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbMatricole);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.nupVoto);
+            this.Controls.Add(this.cmbMaterie);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnInserisciStudente);
+            this.Controls.Add(this.cmbClasse);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtCognome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStudSenzaVoti);
+            this.Controls.Add(this.dgvValutazioni);
+            this.Controls.Add(this.dgvStudenti);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Gestione Studenti";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvStudenti)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvValutazioni)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nupVoto)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvStudenti;
+        private System.Windows.Forms.DataGridView dgvValutazioni;
+        private System.Windows.Forms.Button btnStudSenzaVoti;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCognome;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.ComboBox cmbClasse;
+        private System.Windows.Forms.Button btnInserisciStudente;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbMaterie;
+        private System.Windows.Forms.NumericUpDown nupVoto;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdbLaboratorio;
+        private System.Windows.Forms.RadioButton rdbOrale;
+        private System.Windows.Forms.RadioButton rdbScritto;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbMatricole;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ 2024-3einf/46-GestioneStudenti_Solution/GestioneStudenti/FormMain.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GestioneStudenti
+{
+    public partial class FormMain : Form
+    {
+        string[] datiStudenti =
+        {
+            "1 Barberis Giovanni 1A",
+            "2 Gotta Luigino 2B",
+            "3 Mori Mario 2A",
+            "4 Pio Mimmo 3A",
+            "5 Liberale Patrizia 3B",
+            "6 Gotta Maria 3A",
+            "7 Moramarco Mario 1A",
+            "8 Pio Mimma 2B",
+        };
+
+        public struct studente
+        {
+            public int matricola;   // chiave primaria
+            public string cognome;
+            public string nome;
+            public string classe;
+        }
+
+        studente[] studenti;
+        int nStudenti = 0;
+
+        string[] datiValutazioni =
+        {
+            "Inglese 7 O 1",
+            "Informatica 5 L 2",
+            "Informatica 4 S 3",
+            "Sistemi 9 O 4",
+            "Inglese 5 S 5",
+            "Sistemi 3 L 6",
+            "Sistemi 8 O 7",
+            "Italiano 6 S 8",
+            "Sistemi 8 O 8",
+        };
+
+        public struct valutazione
+        {
+            public string materia;
+            public int voto;
+            public string tipo;
+            public int matricola;
+        };
+
+        valutazione[] valutazioni;
+        int nValutazioni = 0;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(1120, 600);
+            settaDgv(dgvStudenti, "MATRICOLA COGNOME NOME CLASSE");
+            caricaDatiStudenti();
+            nStudenti = datiStudenti.Length;
+            settaDgv(dgvValutazioni, "MATERIA VOTO TIPO MATRICOLA");
+            caricaDatiValutazioni();
+            nValutazioni = datiValutazioni.Length;
+        }
+
+        private void settaDgv(DataGridView dgv, string intestazioni)
+        {
+            string[] intestazioniArray = intestazioni.Split(' ');
+            dgv.ColumnCount = intestazioniArray.Length;
+            for (int i = 0; i < intestazioniArray.Length; i++)
+                dgv.Columns[i].HeaderText = intestazioniArray[i];
+            dgv.RowHeadersVisible = false;
+            dgv.ClearSelection();
+        }
+
+        private void caricaDatiStudenti()
+        {
+            List<string> listClassi = new List<string>();
+            string[] dati = new string[3];
+            for (int i = 0; i < datiStudenti.Length; i++)
+            {
+                nStudenti++;
+                Array.Resize(ref studenti, nStudenti);
+                dati = datiStudenti[i].Split(' ');
+                studenti[nStudenti - 1].matricola = Convert.ToInt32(dati[0]);
+                cmbMatricole.Items.Add(dati[0]);
+                cmbMatricole.SelectedIndex = 0;
+                studenti[nStudenti - 1].cognome = dati[1];
+                studenti[nStudenti - 1].nome = dati[2];
+                studenti[nStudenti - 1].classe = dati[3];
+                dgvStudenti.Rows.Add(dati[0], dati[1], dati[2], dati[3]);
+                if (!listClassi.Contains(dati[3]))
+                    listClassi.Add(dati[3]);
+            }
+            listClassi.Sort();
+            cmbClasse.DataSource = listClassi;
+        }
+
+        private void caricaDatiValutazioni()
+        {
+            string[] dati = new string[3];
+            for (int i = 0; i < datiValutazioni.Length; i++)
+            {
+                nValutazioni++;
+                Array.Resize(ref valutazioni, nValutazioni);
+                dati = datiValutazioni[i].Split(' ');
+                valutazioni[nValutazioni - 1].materia = dati[0];
+                valutazioni[nValutazioni - 1].voto = Convert.ToInt32(dati[1]);
+                valutazioni[nValutazioni - 1].tipo = dati[2];
+                valutazioni[nValutazioni - 1].matricola = Convert.ToInt32(dati[3]);
+                dgvValutazioni.Rows.Add(dati[0], dati[1], dati[2], dati[3]);
+                //Popolo anche cmbMaterie
+                if (!cmbMaterie.Items.Contains(dati[0]))
+                    cmbMaterie.Items.Add(dati[0]);
+            }
+
+        }
+
+        private void btnInserisciStudente_Click(object sender, EventArgs e)
+        {
+            if (txtCognome.Text!="" && txtNome.Text!="" && cmbClasse.Text!="")
+            {
+                nStudenti++;
+                Array.Resize(ref studenti, nStudenti);
+                studenti[nStudenti - 1].matricola = nStudenti;
+                studenti[nStudenti - 1].cognome = txtCognome.Text;
+                studenti[nStudenti - 1].nome = txtNome.Text;
+                studenti[nStudenti - 1].classe = cmbClasse.Text;
+                dgvStudenti.Rows.Add(nStudenti, txtCognome.Text, txtNome.Text, cmbClasse.Text);
+                txtCognome.Text = txtNome.Text = cmbClasse.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Inserisci tutti i dati!", "ATTENZIONE", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
