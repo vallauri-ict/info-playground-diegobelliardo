e042abe9f6d4fae0a6d7f0ff48fb9d243f29876a
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-04-23 10:36:25 +0200

    [4B] SQLAlunni finito

--- 2024-4binf/15-SQL Alunni/SQLQuery1.sql
+++ 2024-4binf/15-SQL Alunni/SQLQuery1.sql
@@ -43,3 +43,40 @@ SELECT MONTH(a.dataNascita) AS MESE_NASCITA,count(*) AS NUM_NATI
 FROM alunni a
 WHERE a.dataNascita is not null
 GROUP BY MONTH(a.dataNascita)
+
+--8.      
+--Per ogni altezza visualizzare quanti sono gli
+--alunni escludendo le altezze con un solo alunnno
+SELECT a.altezza, count(*) AS N_Alunni
+FROM alunni a
+GROUP BY a.altezza
+HAVING count(*) > 1
+
+
+--9.      
+--Visualizzare idAlunno, cognome, nome e
+--‘maggiorenne’ se ha già compiuto 18 anni, ‘quasi maggiorenne’ se nell’anno in
+--corso compie 18 anni, ‘minorenne’ negli altri casi
+SELECT a.Id, a.cognome, a.nome, a.dataNascita , CASE
+    WHEN DATEDIFF(year, a.dataNascita, GETDATE()) > 18 THEN 'maggiorenne'
+    WHEN DATEDIFF(year, a.dataNascita, GETDATE()) = 18 THEN 'quasi maggiorenne'
+    ELSE 'minorenne'
+    END
+FROM alunni a
+
+SELECT a.Id, a.cognome, a.nome, a.dataNascita , CASE
+    WHEN DATEADD(year, 18, a.dataNascita) < GETDATE() THEN 'maggiorenne'
+    WHEN Year(a.dataNascita)+18 = Year(GETDATE()) THEN 'quasi maggiorenne'
+    --WHEN DATEADD(year, 18, a.dataNascita) > GETDATE() 
+    --AND DATEDIFF(year, a.dataNascita, GETDATE()) = 18 THEN 'quasi maggiorenne'
+    ELSE 'minorenne'
+    END
+FROM alunni a
+
+
+--10.  
+--Visualizzare i dati dell’alunno più giovane
+
+SELECT *
+FROM alunni a
+WHERE a.dataNascita = (SELECT MAX(a1.dataNascita) FROM alunni a1)
