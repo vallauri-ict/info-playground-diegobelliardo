15179601d5b257ce8b080f8b8cb2fef14c9208ed
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-09-11 10:27:44 +0200

    [3E] Primo es C# Bolletta telefonica

---
+++ 2025-3einf/01-BollettaTelefonica/01-BollettaTelefonica/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_BollettaTelefonica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroScatti;
+            double costo = 0;
+
+            Console.Write("Inserisci il numero di scatti: ");
+            numeroScatti = Convert.ToInt32(Console.ReadLine());
+
+            if (numeroScatti >= 0)
+            {
+                if (numeroScatti <= 30)
+                {
+                    costo = (numeroScatti * 0.2) + 2.5;
+                }
+                else if (numeroScatti > 30 && numeroScatti <= 100)
+                {
+                    costo = 6 + (numeroScatti - 30) * 0.15 + 2.5;
+                }
+                else
+                {
+                    costo = 16.5 + (numeroScatti - 100) * 0.1 + 2.5;
+                }
+                Console.WriteLine("Costo totale: " + costo);
+            }
+            else
+            {
+                Console.WriteLine("Numero scatti deve essere maggiore o uguale a 0");
+            }
+        }
+    }
+}
