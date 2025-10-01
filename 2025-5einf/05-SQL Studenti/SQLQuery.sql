26f66dbbc1277af8c12f835915ed4d49acd877a0
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-10-01 12:53:49 +0200

    [5E] SQLStudenti

--- 2025-5einf/05-SQL Studenti/SQLQuery.sql
+++ 2025-5einf/05-SQL Studenti/SQLQuery.sql
@@ -44,3 +44,49 @@ AND d.CodDocente=c.CodDocente
 AND d.Dipartimento='Informatica'
 GROUP BY c.CodCorso
 HAVING COUNT(*)>5
+
+-- 11)
+
+SELECT *
+FROM Corsi c
+WHERE NOT EXISTS (SELECT *
+					FROM Frequenta f, Studenti s
+					WHERE s.CorsoLaurea <> 'SBC'
+					AND f.Matricola=s.Matricola
+					AND f.CodCorso=c.CodCorso)
+
+-- 12)
+SELECT d.CodDocente, d.NomeD, count(*) AS Numero_studenti
+FROM Docenti d, Corsi c, Frequenta f
+WHERE d.CodDocente=c.CodDocente
+AND f.CodCorso=c.CodCorso
+GROUP BY d.CodDocente, d.NomeD
+HAVING count(*)>5
+
+-- OPPURE
+SELECT d.CodDocente, d.NomeD
+FROM Docenti d, Corsi c
+WHERE d.CodDocente=c.CodDocente
+AND 5 < (SELECT count(*)
+		FROM Frequenta f
+		WHERE f.CodCorso=c.CodCorso)
+
+-- 13 )
+SELECT *
+FROM Corsi c
+WHERE NOT EXISTS (SELECT *
+					FROM Studenti s
+					WHERE s.CorsoLaurea='SBC'
+					AND Not EXISTS (SELECT *
+									FROM Frequenta f
+									WHERE f.CodCorso=c.CodCorso
+									AND f.Matricola=s.Matricola))
+
+SELECT *
+FROM Studenti s
+WHERE s.CorsoLaurea='SBC'
+
+SELECT *
+FROM Frequenta f
+WHERE f.Matricola IN (1001,1002,1003,1004,1005,1006)
+
