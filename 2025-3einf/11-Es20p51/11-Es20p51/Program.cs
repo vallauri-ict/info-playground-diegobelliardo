c348802c302ec252dd8ee2716a3c51f528a11bdb
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-10-01 11:57:22 +0200

    [3E] Es 20 pag 51

---
+++ 2025-3einf/11-Es20p51/11-Es20p51/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Es20p51
+{
+    internal class Program
+    {
+        static int LeggiNumero(string messaggio)
+        {
+            bool isCorrect = false;
+            int numero;
+
+            do
+            {
+                Console.Write(messaggio);
+                if (int.TryParse(Console.ReadLine(), out numero))
+                {
+                    isCorrect = true;
+                }
+                else
+                {
+                    isCorrect = false;
+                    Console.WriteLine("Devi inserire un numero Intero!!!");
+                }
+            } while (!isCorrect);
+
+            return numero;
+        }
+
+        static void Main(string[] args)
+        {
+            int nGiorniPrestito;
+
+            Console.OutputEncoding = Encoding.UTF8; // Forza l'encoding della console a UTF8
+
+            nGiorniPrestito = LeggiNumero("Inserisci il numero di giorni trascorsi dal prestito: ");
+            if (nGiorniPrestito <= 15)
+            {
+                Console.WriteLine("Non sei, al momento, in multa");
+            }
+            else
+            {
+                int nGiorniMulta = nGiorniPrestito - 15;
+                Console.WriteLine($"Devi pagare una multa di {nGiorniMulta * 0.2}€");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
