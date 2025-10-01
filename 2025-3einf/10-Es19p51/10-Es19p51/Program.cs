d142ac1a7d44e46f338cadc8017a07df7154c64b
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-10-01 11:50:46 +0200

    [3E] Es 19 pag 51

---
+++ 2025-3einf/10-Es19p51/10-Es19p51/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Es19p51
+{
+    internal class Program
+    {
+        static int LeggiNumero(string messaggio)
+        {
+            bool isCorrect = false;
+            int numero;
+
+            do
+            {
+                Console.Write(messaggio);
+                if (int.TryParse(Console.ReadLine(), out numero))
+                {
+                    isCorrect = true;
+                }
+                else
+                {
+                    isCorrect = false;
+                    Console.WriteLine("Devi inserire un numero Intero!!!");
+                }
+            } while (!isCorrect);
+
+            return numero;
+        }
+        static void Main(string[] args)
+        {
+            int nRagazzi, nRagazze;
+            double spesaTotale;
+
+            nRagazzi = LeggiNumero("Inserisci il numero dei ragazzi: ");
+            nRagazze = LeggiNumero("Inserisci il numero delle ragazze: ");
+
+            spesaTotale = nRagazzi * 12 + nRagazze * 12 * 0.7;
+            Console.WriteLine($"La spesa totale è {spesaTotale}");
+            
+            Console.ReadKey();
+        }
+    }
+}
