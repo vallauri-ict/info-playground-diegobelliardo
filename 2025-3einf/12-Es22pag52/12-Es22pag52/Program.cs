1cc87b4ea91a11be2c4ed589974175a53f685f69
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-10-07 08:48:15 +0200

    [3E] Es numeri pari

---
+++ 2025-3einf/12-Es22pag52/12-Es22pag52/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Es22pag52
+{
+    internal class Program
+    {
+        static int nNumeriPari=0;
+        static int cont = 2;
+        const int NUMERI_PARI = 100;
+        static void Main(string[] args)
+        {
+            do
+            {
+                if(cont % 2 == 0)
+                {
+                    nNumeriPari++;
+                    Console.WriteLine($"Numero pari: {cont}");
+                }
+                cont++;
+            } while (nNumeriPari < NUMERI_PARI);
+
+            Console.ReadKey();
+        }
+    }
+}
