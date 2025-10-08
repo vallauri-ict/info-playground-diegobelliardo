1733e9c2a0890b167b816ace05b8cbc41f476d0e
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-10-08 10:43:05 +0200

    [3E] Es 25 pag 52

---
+++ 2025-3einf/13-Es25pag52/13-Es25pag52/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Es25pag52
+{
+    internal class Program
+    {
+        static double media = 0;
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
---
+++ 2025-3einf/14-Es26pag52/14-Es26pag52/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Es26pag52
+{
+    internal class Program
+    {
+        static int fattoriale = 1;
+        static int LeggiNumero(string messaggio,bool soloPositivi=true)
+        {
+            bool isCorrect = false;
+            int numero;
+
+            do
+            {
+                Console.Write(messaggio);
+                if (int.TryParse(Console.ReadLine(), out numero))
+                {
+                    isCorrect = true;
+                }
+                else
+                {
+                    isCorrect = false;
+                    Console.WriteLine("Devi inserire un numero valido!");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                if (isCorrect && soloPositivi && numero <= 0)
+                {
+                    isCorrect = false;
+                    Console.WriteLine("Il prezzo deve essere maggiore di 0");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            } while (!isCorrect);
+
+            return numero;
+        }
+        private static void CalcolaFattoriale(int n)
+        {
+            while (n > 1)
+            {
+                fattoriale = fattoriale * n;
+                n--;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            int n;
+
+            n = LeggiNumero("Inserisci il numero di cui vuoi il fattoriale: ");
+            CalcolaFattoriale(n);
+            Console.WriteLine($"Il fattoriale di {n} vale {fattoriale}");
+
+            Console.ReadKey();
+        }
+
+    }
+}
