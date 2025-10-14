b4b3586643e5a2de8cd9d670c80c6e00c6ddcadc
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-10-14 08:48:48 +0200

    [3E]Es 25 pag 52

--- 2025-3einf/13-Es25pag52/13-Es25pag52/Program.cs
+++ 2025-3einf/13-Es25pag52/13-Es25pag52/Program.cs
@@ -9,10 +9,57 @@ namespace _13_Es25pag52
     internal class Program
     {
         static double media = 0;
+        static int nNumeri;
+
+        /// <summary>
+        /// Legge un numero da tastiera e lo restituisce, se valido.
+        /// </summary>
+        /// <param name="messaggio">Messaggio visualizzato</param>
+        /// <param name="soloPositivi">[Opsionale] Se true accetta solo positivi</param>
+        /// <returns>Numero inserito da tastiera</returns>
+        static int LeggiNumero(string messaggio, bool soloPositivi = true)
+        {
+            bool isCorrect = false;
+            int numero;
+
+            do
+            {
+                Console.Write(messaggio);
+                if (int.TryParse(Console.ReadLine(), out numero))
+                {
+                    isCorrect = true;
+                }
+                else
+                {
+                    isCorrect = false;
+                    Console.WriteLine("Devi inserire un numero valido!");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                if (isCorrect && soloPositivi && numero <= 0)
+                {
+                    isCorrect = false;
+                    Console.WriteLine("Il prezzo deve essere maggiore di 0");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            } while (!isCorrect);
+
+            return numero;
+        }
         static void Main(string[] args)
         {
-            
+            nNumeri = LeggiNumero("Di quanti numeri vuoi fare la media? ");
 
+            for (int i = 0; i < nNumeri; i++)
+            {
+                media += LeggiNumero($"Inserisci il numero {i+1}: ", false);
+            }
+
+            Console.WriteLine($"La media dei numeri è = {media/nNumeri}");
+
+
+            Console.ReadKey();
         }
     }
 }
