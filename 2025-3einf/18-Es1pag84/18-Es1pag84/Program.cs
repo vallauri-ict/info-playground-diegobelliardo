67eb4fab4ab2da67f7703880869ddb820c1bd210
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-10-15 11:56:59 +0200

    [3E] Es 1 pag 84

---
+++ 2025-3einf/18-Es1pag84/18-Es1pag84/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Es1pag84
+{
+    internal class Program
+    {
+        static Random rnd = new Random();
+
+        /// <summary>
+        /// Legge un numero da tastiera e lo restituisce, se valido.
+        /// </summary>
+        /// <param name="messaggio">Messaggio visualizzato</param>
+        /// <param name="soloPositivi">[Opsionale] Se true accetta solo positivi</param>
+        /// <returns>Numero inserito da tastiera</returns>
+        static int LeggiNumero(string messaggio, bool soloPositivi = true)
+        {
+            bool isCorrect = false;
+            int numero;
+
+            do
+            {
+                Console.Write(messaggio);
+                if (int.TryParse(Console.ReadLine(), out numero))
+                {
+                    isCorrect = true;
+                }
+                else
+                {
+                    isCorrect = false;
+                    Console.WriteLine("Devi inserire un numero valido!");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                if (isCorrect && soloPositivi && numero <= 0)
+                {
+                    isCorrect = false;
+                    Console.WriteLine("Il prezzo deve essere maggiore di 0");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            } while (!isCorrect);
+
+            return numero;
+        }
+        private static void CaricaVettore(int[] vettore)
+        {
+            for (int i = 0; i<vettore.Length; i++)
+            {
+                vettore[i] = rnd.Next(100);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int lunghezzaVet = LeggiNumero("Inserisci la dimensione del Vettore: ");
+
+            int[] vettoreSorgente=new int[lunghezzaVet];
+            int[] vettoreDestinazione = new int[lunghezzaVet];
+
+            CaricaVettore(vettoreSorgente);
+        }
+
+    }
+}
