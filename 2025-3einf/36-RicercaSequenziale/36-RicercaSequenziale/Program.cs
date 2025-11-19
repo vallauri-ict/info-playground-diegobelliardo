7203d4dbe93d0303bff22f330982073a5c10ebad
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-11-19 11:53:22 +0100

    [3E] Da terminare Caso 2 Ricerca sequenziale

---
+++ 2025-3einf/36-RicercaSequenziale/36-RicercaSequenziale/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _36_RicercaSequenziale
+{
+    internal class Program
+    {
+        
+        private static int RicercaSequenziale(string[] v, string nome)
+        {
+            int i = 0;
+
+            // while (v[i]!=nome && i<v.Length)
+            while (!(v[i]==nome || i==(v.Length-1)))
+            {
+                i++;
+            }
+            if (v[i] == nome)
+            {
+                return i;
+            }
+            else return -1;
+        }
+        static void Main(string[] args)
+        {
+            string[] v = new string[] 
+                    { "Rossi", "Gallo", "Devirgilio", "Melandri", "Fabbri", "Chiera" };
+
+            Console.WriteLine("Inserisci il nome da cercare: ");
+            string nome = Console.ReadLine();
+
+            int pos = RicercaSequenziale(v, nome);
+
+            if (pos==-1)
+            {
+                Console.WriteLine($"Nome: {nome} NON presente!");
+            }
+            else
+            {
+                Console.WriteLine($"Nome: {nome} TROVATO in posizione: {pos}");
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ 2025-3einf/37-RicercaSequenziale2/37-RicercaSequenziale2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_RicercaSequenziale2
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Ricerca sequenziale per array disordinato e con ripetizioni
+        /// </summary>
+        /// <param name="vettore">Vettore su cui cercare</param>
+        /// <param name="vettorePosizioni">Vettore delle posizioni</param>
+        /// <param name="elemento">Elemento da cercare</param>
+        /// <returns>Quante volte elemento compare in vettore</returns>
+        private static int RicercaSequenziale(string[] vettore, int[] vettorePosizioni, string elemento)
+        {
+            int nElementi = 0;
+
+            for (int i = 0; i < vettore.Length; i++)
+            {
+                if (vettore[i]==elemento)
+                {
+                    vettorePosizioni[nElementi] = i;
+                    nElementi++;
+                }
+            }
+
+            return nElementi;
+        }
+        static void Main(string[] args)
+        {
+            string[] v = new string[]
+                    { "Rossi", "Gallo", "Rossi", "Melandri", "Gallo", "Chiera" };
+            int[] vPos = new int[v.Length];
+
+            Console.Write("Inserisci il nome da cercare: ");
+            string nome = Console.ReadLine();
+
+            if (RicercaSequenziale(v, vPos, nome) == 0)
+            {
+                Console.WriteLine($"Nome: {nome} NON presente!");
+            }
+            else
+            {
+                Console.WriteLine($"Nome: {nome} trovato nelle seguenti posizioni:\n");
+                for (int i = 0; i < vPos.Length; i++)
+                {
+                    Console.Write(vPos[i] + " ");
+                }
+            }
+        }
+
+    }
+}
