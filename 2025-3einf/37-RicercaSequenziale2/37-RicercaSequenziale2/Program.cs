74e1f684dc311cba154af9ba83dc12de81b6927f
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-11-25 08:18:07 +0100

    [3E] Ricerca sequenziale caso 2 terminato

--- 2025-3einf/37-RicercaSequenziale2/37-RicercaSequenziale2/Program.cs
+++ 2025-3einf/37-RicercaSequenziale2/37-RicercaSequenziale2/Program.cs
@@ -14,7 +14,7 @@ namespace _37_RicercaSequenziale2
         /// <param name="vettore">Vettore su cui cercare</param>
         /// <param name="vettorePosizioni">Vettore delle posizioni</param>
         /// <param name="elemento">Elemento da cercare</param>
-        /// <returns>Quante volte elemento compare in vettore</returns>
+        /// <returns>Quante volte elemento compare nel vettore</returns>
         private static int RicercaSequenziale(string[] vettore, int[] vettorePosizioni, string elemento)
         {
             int nElementi = 0;
@@ -33,25 +33,26 @@ namespace _37_RicercaSequenziale2
         static void Main(string[] args)
         {
             string[] v = new string[]
-                    { "Rossi", "Gallo", "Rossi", "Melandri", "Gallo", "Chiera" };
+                    { "Rossi", "Gallo", "Rossi", "Melandri", "Gallo", "Chiera", "Gallo" };
             int[] vPos = new int[v.Length];
 
             Console.Write("Inserisci il nome da cercare: ");
             string nome = Console.ReadLine();
 
-            if (RicercaSequenziale(v, vPos, nome) == 0)
+            int nElementi = RicercaSequenziale(v, vPos, nome);
+            if ( nElementi == 0)
             {
                 Console.WriteLine($"Nome: {nome} NON presente!");
             }
             else
             {
                 Console.WriteLine($"Nome: {nome} trovato nelle seguenti posizioni:\n");
-                for (int i = 0; i < vPos.Length; i++)
+                for (int i = 0; i < nElementi; i++)
                 {
                     Console.Write(vPos[i] + " ");
                 }
             }
+            Console.ReadKey();
         }
-
     }
 }
