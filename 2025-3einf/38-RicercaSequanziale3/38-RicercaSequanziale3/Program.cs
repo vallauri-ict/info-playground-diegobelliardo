015f12a2e4d5e1daa29c3cb4c37c704f742e0004
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-11-25 08:36:00 +0100

    [3E] Ricerca sequenziale caso 3 terminato

---
+++ 2025-3einf/38-RicercaSequanziale3/38-RicercaSequanziale3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _38_RicercaSequanziale3
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Ricerca sequenziale CASO 3: vettore ordinato senza ripetizioni
+        /// </summary>
+        /// <param name="vettore">Elementi su cui cercare</param>
+        /// <param name="nome">Elemento da ricercare</param>
+        /// <returns>Posizione dell'elemento trovato; -1 se non trovato</returns>
+        private static int RicercaSequenziale(string[] vettore, string nome)
+        {
+            int i = 0;
+
+            while (i < vettore.Length && nome.CompareTo(vettore[i]) > 0)
+            {
+                i++;
+            }
+            if (vettore[i] == nome)
+            {
+                return i;
+            }
+            else return -1;
+        }
+        static void Main(string[] args)
+        {
+            const string elemento = "Taricc";
+
+            string[] v = new string[]
+                    { "Bianchi", "Gallo", "Rossi", "Melandri", "Napoli", "Olocco", "Taricco" };
+
+            int pos = RicercaSequenziale(v, elemento);
+            if (pos != -1)
+            {
+                Console.WriteLine($"Nome: {elemento} trovato in posizione {pos}");
+            }
+            else Console.WriteLine($"{elemento} non presente nel vettore");
+        }
+    }
+}
