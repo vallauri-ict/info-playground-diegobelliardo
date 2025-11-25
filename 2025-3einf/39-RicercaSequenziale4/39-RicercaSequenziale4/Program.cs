daf3e6ac0bc96ec49db80607d7183cef77d0389b
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-11-25 08:45:53 +0100

    [3E] Ricerca sequenziale caso 4 da terminare

---
+++ 2025-3einf/39-RicercaSequenziale4/39-RicercaSequenziale4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _39_RicercaSequenziale4
+{
+    /// <summary>
+    /// Ricerca sequenziale per array ordinato e con ripetizioni
+    /// </summary>
+    /// <param name="vettore">Vettore su cui cercare</param>
+    /// <param name="vettorePosizioni">Vettore delle posizioni</param>
+    /// <param name="elemento">Elemento da cercare</param>
+    /// <returns>Quante volte elemento compare nel vettore</returns>
+    private static int RicercaSequenziale(string[] vettore, int[] vettorePosizioni, string elemento)
+    { 
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
