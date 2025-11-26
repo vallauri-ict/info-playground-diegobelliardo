5f7763d037efe146933102d76aa2a9f94bde5ddf
Diego Belliardo <diego.belliardo@vallauri.edu>
2025-11-26 11:53:08 +0100

    [3E] Iniziato es 1 pag. 115

---
+++ 2025-3einf/41-Es1Pag115/41-Es1Pag115/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _41_Es1Pag115
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cognomi = { "Agosti", "Barbieri", "Bianchi", "Colombo", "Conti", "De Luca", "Esposito", "Ferri", "Fontana", "Galli", "Gentile", "Greco", "Lombardi", "Marini", "Martini", "Moretti", "Ricci", "Rossi", "Russo", "Villa" };
+            string[] nomi = { "Alessandro", "Andrea", "Anna", "Chiara", "Daniela", "Davide", "Elena", "Federico", "Filippo", "Francesca", "Giacomo", "Giorgia", "Luca", "Marco", "Martina", "Matteo", "Michele", "Sara", "Simone", "Valentina" };
+            string[] classi = { "1A", "1A", "1A", "1A", "1A", "1A", "1A", "1A", "2A", "2A", "2A", "2A", "3A", "3A", "3A", "2A", "1A", "3A", "1A", "2A" };
+
+
+
+        }
+    }
+}
