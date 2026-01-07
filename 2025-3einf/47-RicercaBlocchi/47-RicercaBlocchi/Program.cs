4967246f1ed52120aaface961f55da75b0282926
Diego Belliardo <diego.belliardo@vallauri.edu>
2026-01-07 12:50:32 +0100

    [3E] Es ricerca a blocchi-DA FINIRE

---
+++ 2025-3einf/47-RicercaBlocchi/47-RicercaBlocchi/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _47_RicercaBlocchi
+{
+    internal class Program
+    {
+        static Random rnd = new Random();
+        bool esci=false;
+        private static int LeggiNumero(string messaggio)
+        {
+            string n;
+            int num = 0;
+            bool ok;
+            do
+            {
+                Console.Write(messaggio);
+                n = Console.ReadLine();
+
+                ok = int.TryParse(n, out num);
+
+                if (num <= 3) ok = false;    // Il vettore deve avere almeno 3 elementi
+
+                if (!ok)
+                {
+                    Console.WriteLine("ERRORE: valore non valido!");
+                }
+            } while (!ok);
+
+            return num;
+        }
+        private static void CaricaVet(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = rnd.Next(100);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            bool trovato = false;
+            int n = LeggiNumero("Dimensione vettore: ");
+            int[] vet = new int[n];
+
+            int elemento = LeggiNumero("Elemento da cercare: ");
+            CaricaVet(vet);
+            double lunghezzaBlocco = Math.Sqrt(n);
+
+        }
+    }
+}
---
+++ 2025-3einf/47-RicercaBlocchi/47-RicercaBlocchi/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente 
+// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+// associate a un assembly.
+[assembly: AssemblyTitle("47-RicercaBlocchi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("47-RicercaBlocchi")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili
+// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da
+// COM, impostare su true l'attributo ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+[assembly: Guid("5386a8d9-f2e9-48ac-92d4-1d980a39ef7e")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+//
+//      Versione principale
+//      Versione secondaria
+//      Numero di build
+//      Revisione
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
