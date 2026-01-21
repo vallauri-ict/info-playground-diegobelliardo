d000ab8c3ffd90e61c8cd39502f2ba58d5d9885a
Diego Belliardo <diego.belliardo@vallauri.edu>
2026-01-21 11:54:24 +0100

    [3E] Es Merge 7-8-10 solo Iniziato pag. 115

---
+++ 2025-3einf/52-Es7pag115/52-Es7pag115/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _52_Es7pag115
+{
+    internal class Program
+    {
+        static string[] nomi = new string[]
+        {
+            "Alessandro",
+            "Andrea",
+            "Francesco",
+            "Giovanni",
+            "Luca",
+            "Marco",
+            "Matteo",
+            "Paolo"
+        };
+
+        static string[] cognomi = new string[]
+        {
+            "Bianchi",
+            "Conti",
+            "Esposito",
+            "Ferrari",
+            "Gallo",
+            "Romano",
+            "Rossi",
+            "Verdi"
+        };
+
+        static string[] nomi2 = new string[]
+        {
+            "Christian",
+            "Daniele",
+            "Davide",
+            "Emanuele",
+            "Francesco",
+            "Gabriele",
+            "Nicola",
+            "Riccardo",
+            "Simone",
+            "Stefano"
+        };
+
+        static string[] cognomi2 = new string[]
+        {
+            "Caruso",
+            "Conti",
+            "Fabbri",
+            "Lombardi",
+            "Marchetti",
+            "Moretti",
+            "Pellegrini",
+            "Rinaldi",
+            "Serra",
+            "Sorrentino"
+        };
+
+        private static void StampaVettore(string[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+        }
+        private static void MergeBase(string[] a, string[] b, string[] c)
+        {
+            int i, j, k;
+
+            i = j = k = 0;
+            do
+            {
+                if (a[i].CompareTo(b[j]) > 0)
+                {
+                    c[k] = b[j];
+                    j++;
+                }
+                else
+                {
+                    if ((a[i].CompareTo(b[j]) == 0))
+                    {
+                        c[k] = b[j];
+                        j++;
+                        i++;
+                    }
+                    else
+                    {
+                        c[k] = a[i];
+                        i++;
+                    }
+                }
+
+                k++;
+            } while (i<a.Length && j<b.Length);
+
+            if (i>=a.Length)
+            {
+                for (; j < b.Length; j++)
+                {
+                    c[k++] = b[j];
+                }
+            }
+            else {
+                for (; i < a.Length; i++)
+                {
+                    c[k++] = a[i];
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string[] nomiCompleto = new string[nomi.Length+nomi2.Length];
+            string[] cognomiCompleto = new string[cognomi.Length + cognomi2.Length];
+
+            MergeBase(nomi, nomi2, nomiCompleto);
+            MergeBase(cognomi, cognomi2, cognomiCompleto);
+
+            StampaVettore(nomi);
+            Console.WriteLine("\n");
+            StampaVettore(nomi2);
+            Console.WriteLine("\n");
+            StampaVettore(nomiCompleto);
+            Console.WriteLine("\n------------------------------------------------");
+            StampaVettore(cognomi);
+            Console.WriteLine("\n");
+            StampaVettore(cognomi2);
+            Console.WriteLine("\n");
+            StampaVettore(cognomiCompleto);
+            Console.WriteLine("\n");
+        }
+
+    }
+}
---
+++ 2025-3einf/53-Es8pag115/53-Es8pag115/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _53_Es8pag115
+{
+    internal class Program
+    {
+        static string[] nomi = new string[]
+        {
+            "Alessandro",
+            "Andrea",
+            "Francesco",
+            "Giovanni",
+            "Luca",
+            "Marco",
+            "Matteo",
+            "Paolo"
+        };
+
+        static string[] cognomi = new string[]
+        {
+            "Bianchi",
+            "Conti",
+            "Esposito",
+            "Ferrari",
+            "Gallo",
+            "Romano",
+            "Rossi",
+            "Verdi"
+        };
+
+        static string[] nomi2 = new string[]
+        {
+            "Christian",
+            "Daniele",
+            "Davide",
+            "Emanuele",
+            "Federico",
+            "Gabriele",
+            "Nicola",
+            "Riccardo",
+            "Simone",
+            "Stefano"
+        };
+
+        static string[] cognomi2 = new string[]
+        {
+            "Caruso",
+            "Correndo",
+            "Fabbri",
+            "Lombardi",
+            "Marchetti",
+            "Moretti",
+            "Pellegrini",
+            "Rinaldi",
+            "Serra",
+            "Sorrentino"
+        };
+
+        private static void StampaVettore(string[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + " ");
+            }
+        }
+        private static void MergeTappo(string[] a, string[] b, string[] c)
+        {
+            int i, j;
+
+            i = j = 0;
+            Array.Resize(ref a, a.Length+1);
+            Array.Resize(ref b, b.Length+1);
+
+            a[a.Length-1] = "Zz";
+            b[b.Length-1] = "Zz";
+
+            for (int k = 0; k < a.Length+b.Length-2; k++)
+            {
+                if (a[i].CompareTo(b[j]) > 0)
+                {
+                    c[k++] = b[j++];
+                }
+                else c[k++] = a[i++];
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] nomiCompleto = new string[nomi.Length + nomi2.Length];
+            string[] cognomiCompleto = new string[cognomi.Length + cognomi2.Length];
+
+            MergeTappo(nomi, nomi2, nomiCompleto);
+            MergeTappo(cognomi, cognomi2, cognomiCompleto);
+
+
+
+            StampaVettore(nomi);
+            Console.WriteLine("\n");
+            StampaVettore(nomi2);
+            Console.WriteLine("\n");
+            StampaVettore(nomiCompleto);
+            Console.WriteLine("\n------------------------------------------------");
+            StampaVettore(cognomi);
+            Console.WriteLine("\n");
+            StampaVettore(cognomi2);
+            Console.WriteLine("\n");
+            StampaVettore(cognomiCompleto);
+            Console.WriteLine("\n");
+        }
+
+    }
+}
