ba40cad1497f55de81cb5253681ec03af9a40c37
Diego Belliardo <diego.belliardo@vallauri.edu>
2026-02-03 08:46:09 +0100

    [3E] Es 12 pag. 115-Da finire

---
+++ 2025-3einf/55-Es12Pag115/55-Es12Pag115/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _55_Es12Pag115
+{
+    internal class Program
+    {
+        static string[] titoli = {
+            "Il nome della rosa",
+            "1984",
+            "Il signore degli anelli",
+            "Harry Potter e la pietra filosofale",
+            "Il piccolo principe",
+            "La coscienza di Zeno",
+            "Il codice Da Vinci",
+            "Orgoglio e pregiudizio",
+            "I promessi sposi",
+            "Fahrenheit 451"
+        };
+
+        static string[] generi = {
+            "Giallo storico",
+            "Fantascienza",
+            "Fantasy",
+            "Fantasy",
+            "Narrativa",
+            "Narrativa",
+            "Thriller",
+            "Romanzo",
+            "Romanzo storico",
+            "Fantascienza"
+        };
+
+        static string[] caseEditrici = {
+            "Bompiani",
+            "Mondadori",
+            "Bompiani",
+            "Salani",
+            "Bompiani",
+            "Newton Compton",
+            "Mondadori",
+            "Feltrinelli",
+            "Garzanti",
+            "Mondadori"
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserire il genere dei lib");
+        }
+    }
+}
