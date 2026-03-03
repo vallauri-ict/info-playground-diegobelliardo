ff968e820535348e9840709d700ea5fafa7a5463
Diego Belliardo <diego.belliardo@vallauri.edu>
2026-03-03 08:50:39 +0100

    [3E] Completato es 26 pag. 116

---
+++ 2025-3einf/62-Es26pag116/62-Es26pag116/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace _62_Es26pag116
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstAnimali = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstSpecie = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstAnimali
+            // 
+            this.lstAnimali.FormattingEnabled = true;
+            this.lstAnimali.ItemHeight = 20;
+            this.lstAnimali.Location = new System.Drawing.Point(12, 32);
+            this.lstAnimali.Name = "lstAnimali";
+            this.lstAnimali.Size = new System.Drawing.Size(148, 264);
+            this.lstAnimali.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Animali";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(190, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Specie";
+            // 
+            // lstSpecie
+            // 
+            this.lstSpecie.FormattingEnabled = true;
+            this.lstSpecie.ItemHeight = 20;
+            this.lstSpecie.Location = new System.Drawing.Point(190, 32);
+            this.lstSpecie.Name = "lstSpecie";
+            this.lstSpecie.Size = new System.Drawing.Size(148, 264);
+            this.lstSpecie.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(124, 315);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 58);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Ordina Per Specie";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 378);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstSpecie);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstAnimali);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstAnimali;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstSpecie;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 2025-3einf/62-Es26pag116/62-Es26pag116/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _62_Es26pag116
+{
+    public partial class Form1 : Form
+    {
+        string[] animali = {
+            "Aquila",
+            "Cane",
+            "Cavallo",
+            "Coccodrillo",
+            "Delfino",
+            "Elefante",
+            "Farfalla",
+            "Gatto",
+            "Rana",
+            "Squalo"
+        };
+
+        string[] specie = {
+            "Uccello",
+            "Mammifero",
+            "Mammifero",
+            "Rettile",
+            "Mammifero",
+            "Mammifero",
+            "Insetto",
+            "Mammifero",
+            "Anfibio",
+            "Pesce"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            lstAnimali.Items.AddRange(animali);
+            lstSpecie.Items.AddRange(specie);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OrdinaPerSpecie(animali,specie);
+            lstAnimali.Items.Clear();
+            lstAnimali.Items.AddRange(animali);
+            lstSpecie.Items.Clear();
+            lstSpecie.Items.AddRange(specie);
+        }
+
+        private void OrdinaPerSpecie(string[] animali, string[] specie)
+        {
+            int posMin;
+            string aus = "";
+
+            for (int i = 0; i < specie.Length; i++)
+            {
+                posMin = i;
+                for (int j = i+1; j < specie.Length; j++)
+                {
+                    if (specie[j].CompareTo(specie[posMin])<0)
+                    {
+                        posMin = j;
+                    }
+                }
+                aus = specie[posMin];
+                specie[posMin] = specie[i];
+                specie[i] = aus;
+                aus = animali[posMin];
+                animali[posMin] = animali[i];
+                animali[i] = aus;
+            }
+        }
+    }
+}
