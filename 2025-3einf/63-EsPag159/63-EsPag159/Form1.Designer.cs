8af9965858bb7e6e97c4a16e0f54c70db64851fb
Diego Belliardo <diego.belliardo@vallauri.edu>
2026-03-25 11:55:02 +0100

    [3E] Iniziato primo es struct

---
+++ 2025-3einf/63-EsPag159/63-EsPag159/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace _63_EsPag159
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstStudenti = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstStudenti
+            // 
+            this.lstStudenti.FormattingEnabled = true;
+            this.lstStudenti.ItemHeight = 16;
+            this.lstStudenti.Location = new System.Drawing.Point(16, 32);
+            this.lstStudenti.Name = "lstStudenti";
+            this.lstStudenti.Size = new System.Drawing.Size(353, 260);
+            this.lstStudenti.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Studenti";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 335);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstStudenti);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstStudenti;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 2025-3einf/63-EsPag159/63-EsPag159/Form1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _63_EsPag159
+{
+    public partial class Form1 : Form
+    {
+        struct Stundete
+        {
+            int matricola;
+            string cognome;
+            string nome;
+            string dataNascita;
+            string classe;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            int nStudenti;
+            string dato;
+            do
+            {
+                dato = Interaction.InputBox("Inserisci numero studenti:");
+
+            } while (!(int.TryParse(dato, out nStudenti)) || nStudenti<=0);
+
+            //MessageBox.Show("Studenti: "+n);
+
+            Stundete[] studenti = new Stundete[nStudenti];
+
+            for (int i = 0; i < nStudenti; i++)
+            {
+                if (i==0)
+                {
+                    dato = Interaction.InputBox("Matricola studente:");
+                }
+                else
+                {
+                    do
+                    {
+                        dato = Interaction.InputBox("Matricola studente:");
+                    }while()
+                }
+            }
+
+        }
+    }
+}
