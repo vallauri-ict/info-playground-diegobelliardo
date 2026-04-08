eb8a2eea7f6f31c9200c1d26bc0c850067c152c8
Diego Belliardo <diego.belliardo@vallauri.edu>
2026-04-08 11:54:52 +0200

    Primo es sui file

---
+++ 2025-3einf/64-EsPag168/P6CaricamentoFile/P6CaricamentoFile/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace P6CaricamentoFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnScrivi = new System.Windows.Forms.Button();
+            this.btnLeggi = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCodice = new System.Windows.Forms.TextBox();
+            this.btnCerca = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnScrivi
+            // 
+            this.btnScrivi.Location = new System.Drawing.Point(13, 13);
+            this.btnScrivi.Name = "btnScrivi";
+            this.btnScrivi.Size = new System.Drawing.Size(75, 31);
+            this.btnScrivi.TabIndex = 0;
+            this.btnScrivi.Text = "Scrivi File";
+            this.btnScrivi.UseVisualStyleBackColor = true;
+            this.btnScrivi.Click += new System.EventHandler(this.btnScrivi_Click);
+            // 
+            // btnLeggi
+            // 
+            this.btnLeggi.Location = new System.Drawing.Point(94, 13);
+            this.btnLeggi.Name = "btnLeggi";
+            this.btnLeggi.Size = new System.Drawing.Size(75, 31);
+            this.btnLeggi.TabIndex = 1;
+            this.btnLeggi.Text = "Leggi File";
+            this.btnLeggi.UseVisualStyleBackColor = true;
+            this.btnLeggi.Click += new System.EventHandler(this.btnLeggi_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Codice Libro:";
+            // 
+            // txtCodice
+            // 
+            this.txtCodice.Location = new System.Drawing.Point(116, 48);
+            this.txtCodice.Name = "txtCodice";
+            this.txtCodice.Size = new System.Drawing.Size(100, 22);
+            this.txtCodice.TabIndex = 3;
+            // 
+            // btnCerca
+            // 
+            this.btnCerca.Location = new System.Drawing.Point(251, 44);
+            this.btnCerca.Name = "btnCerca";
+            this.btnCerca.Size = new System.Drawing.Size(94, 31);
+            this.btnCerca.TabIndex = 4;
+            this.btnCerca.Text = "Cerca Libro";
+            this.btnCerca.UseVisualStyleBackColor = true;
+            this.btnCerca.Click += new System.EventHandler(this.btnCerca_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(395, 106);
+            this.Controls.Add(this.btnCerca);
+            this.Controls.Add(this.txtCodice);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLeggi);
+            this.Controls.Add(this.btnScrivi);
+            this.Name = "Form1";
+            this.Text = "P6-Carica File";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnScrivi;
+        private System.Windows.Forms.Button btnLeggi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCodice;
+        private System.Windows.Forms.Button btnCerca;
+    }
+}
+
---
+++ 2025-3einf/64-EsPag168/P6CaricamentoFile/P6CaricamentoFile/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.VisualBasic;
+
+namespace P6CaricamentoFile
+{
+    struct Libri
+    {
+        public string codLibro;
+        public string titolo;
+        public string autore;
+        public string casaEditrice;
+        public string genere;
+        public string costo;
+
+        public override string ToString()
+        {
+            return $"{codLibro};{titolo};{autore};{casaEditrice};{genere};{costo}";
+        }
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnScrivi_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter("Libri.dat"))
+            {
+                Libri libro = new Libri();
+                string risp = "";
+                do
+                {
+
+                    libro.codLibro = Interaction.InputBox("Inserisci Il Codice:");
+                    libro.titolo = Interaction.InputBox("Inserisci Il Titolo:");
+                    libro.autore = Interaction.InputBox("Inserisci l'autore:");
+                    libro.casaEditrice = Interaction.InputBox("Inserisci la casa editrice:");
+                    libro.genere = Interaction.InputBox("Inserisci Il Genere:");
+                    libro.costo = Interaction.InputBox("Inserisci Il Costo:");
+
+                    sw.WriteLine(libro.ToString());
+
+                    risp = Interaction.InputBox("Vuoi continuare?(S/N)");
+
+                } while (risp.ToUpper() == "S");
+            }
+        }
+
+        private void btnLeggi_Click(object sender, EventArgs e)
+        {
+            using (StreamReader sr = new StreamReader("Libri.dat"))
+            {
+                string riga = "";
+
+                while ((riga = sr.ReadLine()) != null)
+                {
+                    string[] libro = new string[6];
+                    libro = riga.Split(';');
+                    string msg = "Dati del libro:\n";
+                    foreach (string s in libro)
+                    {
+                        msg += s + "\n";
+                    }
+                    MessageBox.Show(msg);
+                }
+            }
+        }
+
+        private void btnCerca_Click(object sender, EventArgs e)
+        {
+            string codice = txtCodice.Text;
+            bool trovato = false;
+            string[] libro = new string[6];
+
+            using (StreamReader sr = new StreamReader("Libri.dat"))
+            {
+                string riga = "";
+
+                while ((riga = sr.ReadLine()) != null && !trovato)
+                {
+                    libro = riga.Split(';');
+                    if (libro[0] == codice)
+                    {
+                        trovato = true;
+                    }
+                }
+            }
+
+            if (trovato)
+            {
+                string msg = "Trovato il libro:\n";
+                foreach (string s in libro)
+                {
+                    msg += s + "\n";
+                }
+                MessageBox.Show(msg);
+            }
+            else MessageBox.Show("Libro non trovato");
+
+        }
+    }
+}
