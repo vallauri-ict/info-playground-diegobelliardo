b9ac48f2af1da0be831a3e0c74b095cdb07c2805
Diego Belliardo <diego.belliardo@vallauri.net>
2026-09-15 09:37:56 +0200

    SQL01

---
+++ 2026-5einf/SQL-Romanzi/SQLQuery2.sql
@@ -0,0 +1,15 @@
+﻿SELECT Nome, Cognome, Titolo
+FROM Autore a, Romanzo r
+WHERE a.Id = r.Autore
+ORDER BY Cognome, Nome
+
+SELECT Nome, Cognome, Titolo, AnnoPubblicazione
+FROM Autore a, Romanzo r
+WHERE a.Id = r.Autore and YEAR(a.DataNascita) BETWEEN 1900 AND 2000
+ORDER BY Cognome, Nome, AnnoPubblicazione
+
+SELECT Titolo
+FROM Romanzo r
+WHERE r.Autore = (SELECT Autore.Id
+FROM Autore
+WHERE Id = r.Autore AND)
